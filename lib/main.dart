8192462223e17d3e72ce48db7e39f1584f2dd5e3
tortamque <90132962+tortamque@users.noreply.github.com>
2023-10-25 18:21:00 +0300

    Connect Firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:poetlum/features/firebase/presentation/widgets/init_crashlytics_widget.dart';
+import 'package:poetlum/features/firebase/presentation/widgets/init_firebase_widget.dart';
+import 'package:poetlum/firebase_options.dart';
 
-void main() {
-  runApp(const PoetlumApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  runApp(
+    InitFirebaseWidget(
+      options: DefaultFirebaseOptions.currentPlatform,
+      child: const InitCrashlyticsWidget(
+        child: PoetlumApp(),
+      ),
+    ),
+  );
 }
 
 class PoetlumApp extends StatelessWidget {
   const PoetlumApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
       title: 'Poetlum',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -17,7 +28,6 @@ class PoetlumApp extends StatelessWidget {
       ),
       home: const PoetlumHomePage(title: 'Poetlum'),
     );
-  }
 }
 
 class PoetlumHomePage extends StatefulWidget {
@@ -31,13 +41,11 @@ class PoetlumHomePage extends StatefulWidget {
 
 class _PoetlumHomePageState extends State<PoetlumHomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Placeholder(),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text(widget.title),
+    ),
+    body: const Placeholder(),
+  );
 }
