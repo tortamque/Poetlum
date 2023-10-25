3e8171d2087fa090fbc0ea0e9d0e9d18f7261ced
tortamque <90132962+tortamque@users.noreply.github.com>
2023-10-25 18:19:37 +0300

    Create custom loader widget

---
+++ lib/features/firebase/presentation/widgets/loaders/loader.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Center(child: CircularProgressIndicator());
+}
