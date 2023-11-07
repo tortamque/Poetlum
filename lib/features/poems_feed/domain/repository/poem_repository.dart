1b1d0f254e2200571ea4021d0687675ae69cb5a0
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-08 00:41:20 +0200

    Create repository

---
+++ lib/features/poems_feed/domain/repository/poem_repository.dart
@@ -0,0 +1,6 @@
+import 'package:poetlum/core/resources/data_state.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+
+abstract class PoemRepository{
+  Future<DataState<List<PoemEntity>>> getPoems();
+}
