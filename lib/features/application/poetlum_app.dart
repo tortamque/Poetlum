2fd980ebbd1d11239a2eb6d8a2e9e7354f7e873a
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-08 13:37:11 +0200

    Add bloc providers

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:poetlum/config/theme/app_theme.dart';
+import 'package:poetlum/core/dependency_injection.dart';
 import 'package:poetlum/features/application/presentation/widgets/AppBar/app_bar.dart';
+import 'package:poetlum/features/poems_feed/presentation/bloc/poem/remote/remote_poem_bloc.dart';
+import 'package:poetlum/features/poems_feed/presentation/bloc/poem/remote/remote_poem_event.dart';
+import 'package:poetlum/features/poems_feed/presentation/pages/home/poems_feed.dart';
 
 class PoetlumApp extends StatelessWidget {
   const PoetlumApp({super.key});
 
   @override
-  Widget build(BuildContext context) => GetMaterialApp(
-      title: 'Poetlum',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  Widget build(BuildContext context) => MultiBlocProvider(
+    providers: [
+      BlocProvider<RemotePoemBloc>(create: (context) => getIt()..add(const GetPoemsEvent())),
+    ],
+    child: GetMaterialApp(
+        title: 'Poetlum',
+        theme: theme(),
+        home: const PoemsFeed(),
       ),
-      home: const PoetlumHomePage(title: 'Poetlum'),
-    );
+  );
 }
 
 class PoetlumHomePage extends StatefulWidget {
