5d14ab5fa25b0231e2b56a9e73e2e15f0aa3686a
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-08 13:36:25 +0200

    Create theme for app

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme()=> ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+  scaffoldBackgroundColor: Colors.white,
+);
