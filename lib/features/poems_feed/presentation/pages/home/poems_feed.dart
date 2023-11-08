fd36213f9c5bd1bbb68f40d75af50f84f3a1edc1
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-08 13:37:50 +0200

    Create poems feed page

---
+++ lib/features/poems_feed/presentation/pages/home/poems_feed.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/application/presentation/widgets/AppBar/app_bar.dart';
+import 'package:poetlum/features/poems_feed/presentation/bloc/poem/remote/remote_poem_bloc.dart';
+import 'package:poetlum/features/poems_feed/presentation/bloc/poem/remote/remote_poem_state.dart';
+
+class PoemsFeed extends StatelessWidget {
+  const PoemsFeed({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: CustomAppBar(
+      title: 'Poetlum',
+      backgroundColor: Colors.grey[350]!,
+    ),
+    body: _buildBody(),
+  );
+
+  BlocBuilder<RemotePoemBloc, RemotePoemState> _buildBody() => BlocBuilder<RemotePoemBloc, RemotePoemState>(
+    builder: (_, state){
+      if(state is RemotePoemLoading){
+        return const Center(child: CircularProgressIndicator(),);
+      } 
+
+      if(state is RemotePoemError){
+        return const Center(child: Icon(Icons.refresh));
+      }
+
+      if(state is RemotePoemDone){
+        return ListView.builder(
+          itemCount: state.poems!.length,
+          itemBuilder: (__, index) {
+            return ListTile(
+              title: Text("${state.poems![index].author}"),
+            );
+          },
+        );
+      }
+
+      return const SizedBox();
+    },
+  );
+}
