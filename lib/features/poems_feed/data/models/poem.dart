5893026c6d06368ad5a94586d9ca8e0cd7f25cb1
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-08 13:37:24 +0200

    Change poem model

--- lib/features/poems_feed/data/models/poem.dart
+++ lib/features/poems_feed/data/models/poem.dart
@@ -1,17 +1,17 @@
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 
-class PoemModel extends PoemEntity{
+class PoemModel extends PoemEntity {
   const PoemModel({
-    String? title,
-    String? author,
-    List<String>? lines,
-    int? linecount,
+    super.title = '',
+    super.author = '',
+    super.lines = const [],
+    super.linecount = 0,
   });
 
   factory PoemModel.fromJson(Map<String, dynamic> json) => PoemModel(
     title: json['title'] ?? '',
     author: json['author'] ?? '',
-    lines: json['lines'] ?? [],
-    linecount: json['linecount'] != null ? int.parse(json['linecount']) : 0,
+    lines: List<String>.from(json['lines'] ?? []),
+    linecount: json['linecount'] != null ? int.parse(json['linecount'].toString()) : 0,
   );
 }
