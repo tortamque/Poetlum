2f092c34475cdb6cf34d7b28b6784665dc084aab
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-10 13:47:46 +0200

    Create password textfield

---
+++ lib/features/registration/presentation/widgets/password_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class PasswordTextField extends StatefulWidget {
+  const PasswordTextField({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool _isPasswordVisible = false;
+  
+  @override
+  Widget build(BuildContext context) => SizedBox(
+    width: MediaQuery.of(context).size.width/1.5,
+    child: TextField(
+      controller: widget.controller,
+      obscureText: !_isPasswordVisible,
+      enableSuggestions: false,
+      autocorrect: false,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        hintText: 'Password',
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+        ),
+        suffixIcon: IconButton(
+        icon: Icon(
+            _isPasswordVisible
+            ? Icons.visibility
+            : Icons.visibility_off,
+          ),
+          onPressed: () => setState(() => _isPasswordVisible = !_isPasswordVisible),
+        ),
+      ),
+    ),
+  );
+}
