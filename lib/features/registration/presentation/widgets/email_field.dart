e5a54a0c3ddf20c49f5f618038fb6eb3fb05ec5a
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-10 13:20:40 +0200

    Create email text field

---
+++ lib/features/registration/presentation/widgets/email_field.dart
@@ -0,0 +1,27 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+
+class EmailField extends StatelessWidget {
+  const EmailField({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+    width: MediaQuery.of(context).size.width/1.5,
+    child: Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: TextFormField(
+        controller: controller,
+        validator: (value) => EmailValidator.validate(value!) ? null : 'Please enter a valid email',
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Email',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    ),
+  );
+}
