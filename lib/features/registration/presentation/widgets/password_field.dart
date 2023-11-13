ccd1e37b1e195d0c5795726885146113600be024
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-13 20:56:57 +0200

    Change password textfield logic

--- lib/features/registration/presentation/widgets/password_field.dart
+++ lib/features/registration/presentation/widgets/password_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/registration/presentation/bloc/validation/validation_cubit.dart';
+import 'package:poetlum/features/registration/presentation/bloc/validation/validation_state.dart';
 
 class PasswordTextField extends StatefulWidget {
   const PasswordTextField({super.key, required this.controller});
@@ -13,26 +16,32 @@ class _PasswordTextFieldState extends State<PasswordTextField> {
   bool _isPasswordVisible = false;
   
   @override
-  Widget build(BuildContext context) => SizedBox(
-    width: MediaQuery.of(context).size.width/1.5,
-    child: TextField(
-      controller: widget.controller,
-      obscureText: !_isPasswordVisible,
-      enableSuggestions: false,
-      autocorrect: false,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(),
-        hintText: 'Password',
-        hintStyle: const TextStyle(
-          color: Colors.grey,
-        ),
-        suffixIcon: IconButton(
-        icon: Icon(
-            _isPasswordVisible
-            ? Icons.visibility
-            : Icons.visibility_off,
+  Widget build(BuildContext context) => BlocBuilder<FormValidationCubit, FormValidationState>(
+    builder: (context, state) => SizedBox(
+      width: MediaQuery.of(context).size.width/1.5,
+      child: TextField(
+        controller: widget.controller,
+        obscureText: !_isPasswordVisible,
+        enableSuggestions: false,
+        autocorrect: false,
+        decoration: InputDecoration(
+          errorMaxLines: 3,
+          errorText: state.passwordValidationState.isValid 
+            ? null
+            : state.passwordValidationState.errorMessage,
+          border: const OutlineInputBorder(),
+          hintText: 'Password',
+          hintStyle: const TextStyle(
+            color: Colors.grey,
           ),
-          onPressed: () => setState(() => _isPasswordVisible = !_isPasswordVisible),
+          suffixIcon: IconButton(
+          icon: Icon(
+              _isPasswordVisible
+              ? Icons.visibility
+              : Icons.visibility_off,
+            ),
+            onPressed: () => setState(() => _isPasswordVisible = !_isPasswordVisible),
+          ),
         ),
       ),
     ),
