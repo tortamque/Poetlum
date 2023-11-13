15a49f5ad507109402549dcf1ed397b73e0da9a7
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-13 18:55:26 +0200

    Rewrite username textfield

--- lib/features/registration/presentation/widgets/username_field.dart
+++ lib/features/registration/presentation/widgets/username_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/registration/presentation/bloc/validation_cubit.dart';
+import 'package:poetlum/features/registration/presentation/bloc/validation_state.dart';
 
 class UsernameTextField extends StatelessWidget {
   const UsernameTextField({super.key, required this.controller});
@@ -6,19 +9,37 @@ class UsernameTextField extends StatelessWidget {
   final TextEditingController controller;
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-    width: MediaQuery.of(context).size.width/1.5,
-    child: TextField(
-      controller: controller,
-      enableSuggestions: false,
-      autocorrect: false,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: 'Username',
-        hintStyle: TextStyle(
-          color: Colors.grey,
+  Widget build(BuildContext context) => BlocBuilder<FormValidationCubit, FormValidationState>(
+    builder: (context, state){
+      String? errorText;
+
+      if(state.usernameValidationState.state == UsernameValidationStates.correct || state.usernameValidationState.username.isEmpty){
+        errorText = null;
+      } else if(state.usernameValidationState.state == UsernameValidationStates.tooShort){
+        errorText = 'The username lenght is too short';
+      } else if(state.usernameValidationState.state == UsernameValidationStates.tooLong){
+        errorText = 'The username lenght is too long';
+      }else{
+        errorText = 'Username must contain only letters and numbers';
+      }
+
+      return SizedBox(
+        width: MediaQuery.of(context).size.width/1.5,
+        child: TextField(
+          controller: controller,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: InputDecoration(
+            errorText: errorText,
+            errorMaxLines: 3,
+            border: const OutlineInputBorder(),
+            hintText: 'Username',
+            hintStyle: const TextStyle(
+              color: Colors.grey,
+            ),
+          ),
         ),
-      ),
-    ),
+      );
+    }
   );
 }
