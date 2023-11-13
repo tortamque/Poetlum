1bd5c8808800f48566c9555e77d64aeda80a4c43
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-13 20:56:14 +0200

    Rework validation process

--- lib/features/registration/presentation/bloc/validation/validation_cubit.dart
+++ lib/features/registration/presentation/bloc/validation/validation_cubit.dart
@@ -1,78 +1,49 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/features/registration/presentation/bloc/validation/validation_state.dart';
+import 'package:poetlum/features/registration/presentation/bloc/validation/validators.dart';
 
 class FormValidationCubit extends Cubit<FormValidationState> {
-  FormValidationCubit() : super(const FormValidationState());
+  FormValidationCubit({
+    required this.usernameValidator, 
+    required this.emailValidator, 
+    required this.passwordValidator,
+  })
+    : super(const FormValidationState());
+
+  final Validator<String> usernameValidator;
+  final Validator<String> emailValidator;
+  final Validator<String> passwordValidator;
 
   void usernameChanged(String value) {
-    final usernameValid = _isUsernameValid(value);
-    late UsernameValidationStates validationResult;
-
-    if(value.length < 5){
-      validationResult = UsernameValidationStates.tooShort;
-    } else if(value.length > 30){
-      validationResult = UsernameValidationStates.tooLong;
-    } else if(!RegExp(r'^[a-zA-Z0-9]+$').hasMatch(value)){
-      validationResult = UsernameValidationStates.inappropriateChars;
-    } else{
-      validationResult = UsernameValidationStates.correct;
-    }
-
+    final result = usernameValidator.validate(value);
     emit(state.copyWith(
-      usernameValidationState: UsernameValidationState(
-        username: value,
-        isUsernameValid: usernameValid,
-        state: validationResult,
+      usernameValidationState: ValidationState(
+        value: value,
+        isValid: result.isValid,
+        errorMessage: result.errorMessage,
       ),
     ),);
   }
 
   void emailChanged(String value) {
-    final emailValid = _isEmailValid(value);
-    late EmailValidationStates validationResult;
-
-    if(!EmailValidator.validate(value)){
-      validationResult = EmailValidationStates.wrongFormat;
-    } else if(value.length > 40){
-      validationResult = EmailValidationStates.tooLong;
-    } else{
-      validationResult = EmailValidationStates.correct;
-    }
-
+    final result = emailValidator.validate(value);
     emit(state.copyWith(
-      emailValidationState: EmailValidationState(
-        email: value,
-        isEmailValid: emailValid,
-        state: validationResult,
+      emailValidationState: ValidationState(
+        value: value,
+        isValid: result.isValid,
+        errorMessage: result.errorMessage,
       ),
     ),);
   }
 
   void passwordChanged(String value) {
-    final passwordValid = _isPasswordValid(value);
-    late PasswordValidationStates validationResult;
-
-    if(value.length < 8){
-      validationResult = PasswordValidationStates.tooShort;
-    } else if(value.length > 20){
-      validationResult = PasswordValidationStates.tooLong;
-    } else{
-      validationResult = PasswordValidationStates.correct;
-    }
-
+    final result = passwordValidator.validate(value);
     emit(state.copyWith(
-      passwordValidationState: PasswordValidationState(
-        password: value,
-        isPasswordValid: passwordValid,
-        state: validationResult,
+      passwordValidationState: ValidationState(
+        value: value,
+        isValid: result.isValid,
+        errorMessage: result.errorMessage,
       ),
     ),);
   }
-
-  bool _isUsernameValid(String username) => RegExp(r'^[a-zA-Z0-9]+$').hasMatch(username) && username.length >= 5 && username.length <= 30;
-
-  bool _isEmailValid(String email) => EmailValidator.validate(email) && email.length <= 40;
-
-  bool _isPasswordValid(String password) => password.length >= 8 && password.length <= 20;
 }
--- lib/features/registration/presentation/bloc/validation/validation_state.dart
+++ lib/features/registration/presentation/bloc/validation/validation_state.dart
@@ -2,21 +2,21 @@ import 'package:equatable/equatable.dart';
 
 class FormValidationState extends Equatable {
   const FormValidationState({
-    this.emailValidationState = const EmailValidationState(),
-    this.usernameValidationState = const UsernameValidationState(),
-    this.passwordValidationState = const PasswordValidationState(),
+    this.emailValidationState = const ValidationState(),
+    this.usernameValidationState = const ValidationState(),
+    this.passwordValidationState = const ValidationState(),
     this.isFormValid = false,
   });
 
-  final EmailValidationState emailValidationState;
-  final UsernameValidationState usernameValidationState;
-  final PasswordValidationState passwordValidationState;
+  final ValidationState emailValidationState;
+  final ValidationState usernameValidationState;
+  final ValidationState passwordValidationState;
   final bool isFormValid;
 
   FormValidationState copyWith({
-    EmailValidationState? emailValidationState,
-    UsernameValidationState? usernameValidationState,
-    PasswordValidationState? passwordValidationState,
+    ValidationState? emailValidationState,
+    ValidationState? usernameValidationState,
+    ValidationState? passwordValidationState,
     bool? isFormValid,
   }) => FormValidationState(
     emailValidationState: emailValidationState ?? this.emailValidationState,
@@ -34,96 +34,27 @@ class FormValidationState extends Equatable {
   ];
 }
 
-enum UsernameValidationStates{
-  tooShort,
-  tooLong,
-  inappropriateChars,
-  correct,
-}
-
-class UsernameValidationState extends Equatable {
-  const UsernameValidationState({
-    this.username = '',
-    this.state = UsernameValidationStates.tooShort,
-    this.isUsernameValid = false,
+class ValidationState extends Equatable {
+  const ValidationState({
+    this.value = '',
+    this.isValid = false,
+    this.errorMessage,
   });
 
-  final String username;
-  final UsernameValidationStates state;
-  final bool isUsernameValid;
+  final String value;
+  final bool isValid;
+  final String? errorMessage;
 
   @override
-  List<Object?> get props => [username, state, isUsernameValid];
+  List<Object?> get props => [value, isValid, errorMessage];
 
-  UsernameValidationState copyWith({
-    String? username,
-    UsernameValidationStates? state,
-    bool? isUsernameValid,
-  }) => UsernameValidationState(
-    username: username ?? this.username,
-    state: state ?? this.state,
-    isUsernameValid: isUsernameValid ?? this.isUsernameValid,
-  );
-}
-
-enum PasswordValidationStates{
-  tooShort,
-  tooLong,
-  correct,
-}
-
-class PasswordValidationState extends Equatable {
-  const PasswordValidationState({
-    this.password = '',
-    this.state = PasswordValidationStates.tooShort,
-    this.isPasswordValid = false,
-  });
-
-  final String password;
-  final PasswordValidationStates state;
-  final bool isPasswordValid;
-
-  @override
-  List<Object?> get props => [password, state, isPasswordValid];
-
-  PasswordValidationState copyWith({
-    String? password,
-    PasswordValidationStates? state,
-    bool? isPasswordValid,
-  }) => PasswordValidationState(
-    password: password ?? this.password,
-    state: state ?? this.state,
-    isPasswordValid: isPasswordValid ?? this.isPasswordValid,
-  );
-}
-
-enum EmailValidationStates{
-  wrongFormat,
-  tooLong,
-  correct,
-}
-
-class EmailValidationState extends Equatable {
-  const EmailValidationState({
-    this.email = '',
-    this.state = EmailValidationStates.wrongFormat,
-    this.isEmailValid = false,
-  });
-
-  final String email;
-  final EmailValidationStates state;
-  final bool isEmailValid;
-
-  @override
-  List<Object?> get props => [email, state, isEmailValid];
-
-  EmailValidationState copyWith({
-    String? email,
-    EmailValidationStates? state,
-    bool? isEmailValid,
-  }) => EmailValidationState(
-    email: email ?? this.email,
-    state: state ?? this.state,
-    isEmailValid: isEmailValid ?? this.isEmailValid,
+  ValidationState copyWith({
+    String? value,
+    bool? isValid,
+    String? errorMessage,
+  }) => ValidationState(
+    value: value ?? this.value,
+    isValid: isValid ?? this.isValid,
+    errorMessage: errorMessage ?? this.errorMessage,
   );
 }
