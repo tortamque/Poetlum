6f3ce1e01b3bb008ffcd1ba60fce3678502ae6b3
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-13 22:49:31 +0200

    Move register button to a separate widget

--- lib/features/registration/presentation/pages/registration/registration_page.dart
+++ lib/features/registration/presentation/pages/registration/registration_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:poetlum/features/registration/presentation/bloc/registation/register_cubit.dart';
-import 'package:poetlum/features/registration/presentation/bloc/registation/register_state.dart';
 import 'package:poetlum/features/registration/presentation/bloc/validation/validation_cubit.dart';
-import 'package:poetlum/features/registration/presentation/bloc/validation/validation_state.dart';
 import 'package:poetlum/features/registration/presentation/widgets/email_field.dart';
 import 'package:poetlum/features/registration/presentation/widgets/password_field.dart';
+import 'package:poetlum/features/registration/presentation/widgets/register_button.dart';
 import 'package:poetlum/features/registration/presentation/widgets/username_field.dart';
 
 class RegistrationPage extends StatelessWidget {
@@ -34,103 +31,46 @@ class RegistrationPage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: BlocBuilder<FormValidationCubit, FormValidationState>(
-          builder: (context, validationState){
-            print(validationState.isFormValid);
-            return Row(
-              children: [
-                Expanded(
-                  child: Column(
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  const Spacer(),
+                  const Text(
+                    'Registration',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  const Spacer(flex: 2,),
+                
+                  UsernameTextField(controller: _usernameController),
+                  const Spacer(),
+                
+                  EmailTextField(controller: _emailController),
+                  const Spacer(),
+                
+                  PasswordTextField(controller: _passwordController),
+                  const Spacer(flex: 2,),
+                
+                  const RegisterButton(),
+                  const Spacer(),
+                
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Spacer(),
-                      const Text(
-                        'Registration',
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      const Spacer(flex: 2,),
-                    
-                      UsernameTextField(controller: _usernameController),
-                      const Spacer(),
-                    
-                      EmailTextField(controller: _emailController),
-                      const Spacer(),
-                    
-                      PasswordTextField(controller: _passwordController),
-                      const Spacer(flex: 2,),
-                    
-                      BlocConsumer<RegisterCubit, RegisterState>(
-                        listener: (context, registerState) {
-                          if (registerState.status == RegisterStatus.success) {
-                            _showPositiveToast();
-                          } else if (registerState.status == RegisterStatus.error) {
-                            _showNegativeToast(registerState.errorMessage ?? 'Unknown error');
-                          }
-                        },
-                        builder: (context, state) => state.status == RegisterStatus.submitting 
-                          ? const CircularProgressIndicator()
-                          : FilledButton.tonal(
-                            onPressed: validationState.isFormValid
-                              ? () {
-                                  context.read<RegisterCubit>().register(
-                                    _usernameController.text,
-                                    _emailController.text,
-                                    _passwordController.text,
-                                  );
-                                }
-                              : null,
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), 
-                              child: Text(
-                                'Register',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ),
-                          ),
-                      ),
-                      const Spacer(),
-                    
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Already have an account?'),
-                          TextButton(onPressed: (){}, child: const Text('Login', style: TextStyle(decoration: TextDecoration.underline),),),
-                        ],
-                      ),
-                      const Spacer(flex: 12,),
+                      const Text('Already have an account?'),
+                      TextButton(onPressed: (){}, child: const Text('Login', style: TextStyle(decoration: TextDecoration.underline),),),
                     ],
                   ),
-                ),
-              ],
-            );
-          },
+                  const Spacer(flex: 12,),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Future<void> _showPositiveToast() async{
-    await Fluttertoast.showToast(
-      msg: 'Your registration was successful',
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.green,
-      textColor: Colors.white,
-      fontSize: 16,
-    );
-  }
-
-  Future<void> _showNegativeToast(String error) async{
-    await Fluttertoast.showToast(
-      msg: error,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.red,
-      textColor: Colors.white,
-      fontSize: 16,
     );
   }
 }
