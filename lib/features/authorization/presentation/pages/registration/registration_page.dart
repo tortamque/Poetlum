a329ede062e6e9ffcc1dce4fe9163e8787d5a56e
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-16 18:48:34 +0200

    Rework registration page

--- lib/features/authorization/presentation/pages/registration/registration_page.dart
+++ lib/features/authorization/presentation/pages/registration/registration_page.dart
@@ -9,6 +9,86 @@ import 'package:poetlum/features/authorization/presentation/widgets/password_fie
 import 'package:poetlum/features/authorization/presentation/widgets/register_button.dart';
 import 'package:poetlum/features/authorization/presentation/widgets/username_field.dart';
 
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+    height: MediaQuery.of(context).size.height/10,
+    child: const Column(
+      children: [
+        Spacer(),
+        Text(
+          'Registration',
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        Spacer(),
+      ],
+    ),
+  );
+}
+
+class _Form extends StatelessWidget {
+  const _Form(this.usernameController, this.emailController, this.passwordController);
+
+  final TextEditingController usernameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) => BlocBuilder<RegisterFormValidationCubit, RegisterFormValidationState>(
+    builder: (context, state) => SizedBox(
+      height: MediaQuery.of(context).size.height/2.75,
+      child: Column(
+        children: [
+          UsernameTextField(controller: usernameController),
+            const Spacer(),
+          
+            EmailTextField<RegisterFormValidationCubit, RegisterFormValidationState>(controller: emailController),
+            const Spacer(),
+          
+            PasswordTextField<RegisterFormValidationCubit, RegisterFormValidationState>(controller: passwordController),
+            const Spacer(),
+          
+            AuthButton<
+              AuthCubit, 
+              AuthState, 
+              RegisterFormValidationCubit, 
+              RegisterFormValidationState
+            >(
+              isEnabled: state.isFormValid,
+              text: 'Register',
+              successfulToastText: 'Your registration was successful',
+              onPressed: () => context.read<AuthCubit>().register(
+                username: state.usernameValidationState.value,
+                email: state.emailValidationState.value,
+                password: state.passwordValidationState.value,
+              ),
+            ),
+        ],
+      ),
+    ),
+  );
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer();
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+    height: MediaQuery.of(context).size.height/10,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Already have an account?'),
+        TextButton(onPressed: (){}, child: const Text('Login', style: TextStyle(decoration: TextDecoration.underline),),),
+      ],
+    ),
+  );
+}
+
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
 
@@ -44,57 +124,17 @@ class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) => Scaffold(
       body: SafeArea(
         child: BlocBuilder<RegisterFormValidationCubit, RegisterFormValidationState>(
-          builder: (context, state) => Row(
+          builder: (context, state) => Column(
             children: [
-              Expanded(
-                child: Column(
-                  children: [
-                    const Spacer(),
-                    const Text(
-                      'Registration',
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
-                    ),
-                    const Spacer(flex: 2,),
-                  
-                    UsernameTextField(controller: _usernameController),
-                    const Spacer(),
-                  
-                    EmailTextField<RegisterFormValidationCubit, RegisterFormValidationState>(controller: _emailController),
-                    const Spacer(),
-                  
-                    PasswordTextField<RegisterFormValidationCubit, RegisterFormValidationState>(controller: _passwordController),
-                    const Spacer(flex: 2,),
-                  
-                    AuthButton<
-                      AuthCubit, 
-                      AuthState, 
-                      RegisterFormValidationCubit, 
-                      RegisterFormValidationState
-                    >(
-                      isEnabled: state.isFormValid,
-                      text: 'Register',
-                      successfulToastText: 'Your registration was successful',
-                      onPressed: () => context.read<AuthCubit>().register(
-                        username: state.usernameValidationState.value,
-                        email: state.emailValidationState.value,
-                        password: state.passwordValidationState.value,
-                      ),
-                    ),
-                    const Spacer(),
-                  
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Already have an account?'),
-                        TextButton(onPressed: (){}, child: const Text('Login', style: TextStyle(decoration: TextDecoration.underline),),),
-                      ],
-                    ),
-                    const Spacer(flex: 12,),
-                  ],
-                ),
+              const _Header(),
+            
+              _Form(
+                _usernameController,
+                _emailController,
+                _passwordController,
               ),
+            
+              const _Footer(),
             ],
           ),
         ),
