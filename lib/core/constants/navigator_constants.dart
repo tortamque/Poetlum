b7c9ef22c95caaa7aa08b5bfcceb30a4690c1e77
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-18 15:14:33 +0200

    Create constants files

---
+++ lib/core/constants/navigator_constants.dart
@@ -0,0 +1,4 @@
+const String authWrapperPageConstant = '/auth_wrapper';
+const String registerPageConstant = '/register';
+const String loginPageConstant = '/login';
+const String poemsFeedPageConstant = '/poems_feed';
