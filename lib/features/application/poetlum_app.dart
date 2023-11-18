bb97768a6a046d99155363615f4ab15201d82949
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-18 15:17:10 +0200

    Create routes

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:poetlum/config/theme/app_theme.dart';
+import 'package:poetlum/core/constants/navigator_constants.dart';
 import 'package:poetlum/features/application/presentation/widgets/auth_wrapper.dart';
+import 'package:poetlum/features/authorization/presentation/pages/login/login_page.dart';
+import 'package:poetlum/features/authorization/presentation/pages/registration/registration_page.dart';
 import 'package:poetlum/features/multi_bloc_provider/presentation/init_blocs.dart';
+import 'package:poetlum/features/poems_feed/presentation/pages/home/poems_feed.dart';
 
 class PoetlumApp extends StatelessWidget {
   const PoetlumApp({super.key});
@@ -12,7 +16,13 @@ class PoetlumApp extends StatelessWidget {
     child: GetMaterialApp(
       title: 'Poetlum',
       theme: theme(),
-      home: const AuthWrapper(),
+      initialRoute: authWrapperPageConstant,
+      routes: {
+        authWrapperPageConstant:(_) => const AuthWrapper(),
+        registerPageConstant: (_) => const RegistrationPage(),
+        loginPageConstant: (_) => const LoginPage(),
+        poemsFeedPageConstant: (_) => const PoemsFeed(),
+      },
     ),
   );
 }
