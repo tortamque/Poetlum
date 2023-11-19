3ada27e172ac828081730aa998bd9744266100e4
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-19 16:33:21 +0200

    Create new usecase

--- lib/features/poems_feed/domain/usecases/get_poems_usecase.dart
+++ lib/features/poems_feed/domain/usecases/get_poems_usecase.dart
@@ -11,3 +11,43 @@ class GetInitialPoemsUseCase implements UseCase<DataState<List<PoemEntity>>, voi
   @override
   Future<DataState<List<PoemEntity>>> call({void params}) => _poemRepository.getInitialPoems();
 }
+
+class GetPoemsUseCaseParams {
+  const GetPoemsUseCaseParams({
+    this.author,
+    this.title,
+    this.lineCount,
+    this.poemCount,
+  });
+
+  final String? author;
+  final String? title;
+  final String? lineCount;
+  final String? poemCount;
+}
+
+class GetPoemsUseCase implements UseCase<DataState<List<PoemEntity>>, GetPoemsUseCaseParams>{
+  GetPoemsUseCase(this._poemRepository);
+
+  final PoemRepository _poemRepository;
+  
+  @override
+  Future<DataState<List<PoemEntity>>> call({GetPoemsUseCaseParams? params}){
+    if(
+      params != null &&
+      (params.author != '' &&
+      params.title != '' &&
+      params.lineCount != '' &&
+      params.poemCount != '')
+    ){
+      return _poemRepository.getPoems(
+        author: params.author,
+        title: params.title,
+        lineCount: params.lineCount,
+        poemCount: params.poemCount,
+      );
+    } else{
+      return _poemRepository.getInitialPoems();
+    }
+  } 
+}
