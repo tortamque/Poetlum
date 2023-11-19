4dc654ee848218fb3dd8cd2e11a417c759cf0c40
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-19 16:32:51 +0200

    Update poem repo

--- lib/features/poems_feed/data/repository/poem_repository_impl.dart
+++ lib/features/poems_feed/data/repository/poem_repository_impl.dart
@@ -32,4 +32,53 @@ class PoemRepositoryImpl implements PoemRepository{
       return DataFailed(error);
     }
   }
+
+  @override
+  Future<DataState<List<PoemModel>>> getPoems({String? author, String? title, String? lineCount, String? poemCount}) async {
+    print('repo $author, $title, $lineCount, $poemCount');
+
+    final outputFields = <String>[];
+    final searchTerms = <String>[];
+
+    if (author != null && author.isNotEmpty) {
+      outputFields.add('author');
+      searchTerms.add(author);
+    }
+    if (title != null && title.isNotEmpty) {
+      outputFields.add('title');
+      searchTerms.add(title);
+    }
+    if (lineCount != null && lineCount.isNotEmpty) {
+      outputFields.add('linecount');
+      searchTerms.add(lineCount);
+    }
+    if (poemCount != null && poemCount.isNotEmpty) {
+      outputFields.add('poemcount');
+      searchTerms.add(poemCount);
+    }
+
+    final outputFieldsPart = outputFields.join(',');
+    final searchTermsPart = searchTerms.join(';');
+    final query = '$outputFieldsPart/$searchTermsPart';
+    print('query $query');
+
+    try {
+      final httpResponse = await _poemApiService.getPoems(query);
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            requestOptions: httpResponse.response.requestOptions,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            error: httpResponse.response.statusMessage,
+          ),
+        );
+      }
+    } on DioException catch (error) {
+      return DataFailed(error);
+    }
+  }
 }
--- lib/features/poems_feed/domain/repository/poem_repository.dart
+++ lib/features/poems_feed/domain/repository/poem_repository.dart
@@ -3,4 +3,5 @@ import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 
 abstract class PoemRepository{
   Future<DataState<List<PoemEntity>>> getInitialPoems();
+  Future<DataState<List<PoemEntity>>> getPoems({String? author, String? title, String? lineCount, String? poemCount});
 }
