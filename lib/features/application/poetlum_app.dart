6a4b0b630d26e53e455aa1187092e67c47d73182
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-22 14:44:10 +0200

    Replace helper widget

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -5,7 +5,6 @@ import 'package:poetlum/core/constants/navigator_constants.dart';
 import 'package:poetlum/features/application/presentation/widgets/auth_wrapper.dart';
 import 'package:poetlum/features/authorization/presentation/pages/login/login_page.dart';
 import 'package:poetlum/features/authorization/presentation/pages/registration/registration_page.dart';
-import 'package:poetlum/features/multi_bloc_provider/presentation/init_blocs.dart';
 import 'package:poetlum/features/poems_feed/presentation/pages/home/poems_feed.dart';
 import 'package:poetlum/features/poems_feed/presentation/pages/poem_view/poem_view.dart';
 import 'package:poetlum/features/saved_poems/presentation/pages/saved_poems_page.dart';
@@ -14,19 +13,17 @@ class PoetlumApp extends StatelessWidget {
   const PoetlumApp({super.key});
 
   @override
-  Widget build(BuildContext context) => InitBlocs(
-    child: GetMaterialApp(
-      title: 'Poetlum',
-      theme: theme(),
-      initialRoute: authWrapperPageConstant,
-      routes: {
-        authWrapperPageConstant:(_) => const AuthWrapper(),
-        registerPageConstant: (_) => const RegistrationPage(),
-        loginPageConstant: (_) => const LoginPage(),
-        poemsFeedPageConstant: (_) => const PoemsFeedPage(),
-        poemViewPageConstant:(_) => const PoemViewPage(),
-        savedPoemsPageConstant:(_) => const SavedPoemsPage(),
-      },
-    ),
+  Widget build(BuildContext context) => GetMaterialApp(
+    title: 'Poetlum',
+    theme: theme(),
+    initialRoute: authWrapperPageConstant,
+    routes: {
+      authWrapperPageConstant:(_) => const AuthWrapper(),
+      registerPageConstant: (_) => const RegistrationPage(),
+      loginPageConstant: (_) => const LoginPage(),
+      poemsFeedPageConstant: (_) => const PoemsFeedPage(),
+      poemViewPageConstant:(_) => const PoemViewPage(),
+      savedPoemsPageConstant:(_) => const SavedPoemsPage(),
+    },
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:poetlum/features/application/poetlum_app.dart';
 import 'package:poetlum/features/dependency_injection/presentation/widgets/init_dependencies.dart';
 import 'package:poetlum/features/firebase/presentation/widgets/init_crashlytics_widget.dart';
 import 'package:poetlum/features/firebase/presentation/widgets/init_firebase_widget.dart';
+import 'package:poetlum/features/multi_bloc_provider/presentation/init_blocs.dart';
 import 'package:poetlum/firebase_options.dart';
 
 void main() async {
@@ -13,7 +14,9 @@ void main() async {
       child: InitFirebaseWidget(
         options: DefaultFirebaseOptions.currentPlatform,
         child: const InitCrashlyticsWidget(
-          child: PoetlumApp(),
+          child: InitBlocs(
+            child: PoetlumApp()
+          ),
         ),
       ),
     ),
