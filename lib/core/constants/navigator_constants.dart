f9c8a29a63fd899ca82f1c82413ef47179515b21
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-22 11:29:12 +0200

    Add page constant

--- lib/core/constants/navigator_constants.dart
+++ lib/core/constants/navigator_constants.dart
@@ -2,3 +2,4 @@ const String authWrapperPageConstant = '/auth_wrapper';
 const String registerPageConstant = '/register';
 const String loginPageConstant = '/login';
 const String poemsFeedPageConstant = '/poems_feed';
+const String poemViewPageConstant = '/poem_view';
