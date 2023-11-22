f69b69afef32da4bfc527c57e12d02569acc9f1c
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-22 14:45:26 +0200

    Move AuthWrapper to another folder

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:poetlum/config/theme/app_theme.dart';
 import 'package:poetlum/core/constants/navigator_constants.dart';
-import 'package:poetlum/features/application/presentation/widgets/auth_wrapper.dart';
+import 'package:poetlum/features/application/presentation/pages/auth_wrapper.dart';
 import 'package:poetlum/features/authorization/presentation/pages/login/login_page.dart';
 import 'package:poetlum/features/authorization/presentation/pages/registration/registration_page.dart';
 import 'package:poetlum/features/poems_feed/presentation/pages/home/poems_feed.dart';
