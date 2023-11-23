251e6d56f9618466ad1571b904c44132eff3b165
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-23 21:52:11 +0200

    Extend API service

--- lib/features/saved_poems/data/data_sources/remote/firebase_api_service.dart
+++ lib/features/saved_poems/data/data_sources/remote/firebase_api_service.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:poetlum/features/poems_feed/data/models/poem.dart';
+import 'package:poetlum/features/saved_poems/data/models/collection.dart';
 
 abstract class FirebaseDatabaseService{
   Future<List<PoemModel>?> getUserPoems(String userId);
+  Future<List<CollectionModel>?> getUserCollections(String userId);
 }
 
 class FirebaseDatabaseServiceImpl implements FirebaseDatabaseService {
@@ -15,13 +17,34 @@ class FirebaseDatabaseServiceImpl implements FirebaseDatabaseService {
       final poems = <PoemModel>[];
 
       
-      for (var child in snapshot.children) {
+      for (final child in snapshot.children) {
         final poemData = Map<String, dynamic>.from(child.value as Map);
         
         poems.add(PoemModel.fromFirebase(poemData));
       }
 
       return poems;
+    } else {
+      return null;
+    }
+  }
+  
+  @override
+  Future<List<CollectionModel>?> getUserCollections(String userId) async{
+    final ref = FirebaseDatabase.instance.ref('$userId/collections');
+    final snapshot = await ref.get();
+
+    if (snapshot.exists) {
+      final collections = <CollectionModel>[];
+
+      
+      for (final child in snapshot.children) {
+        final collectionData = Map<String, dynamic>.from(child.value as Map);
+        
+        collections.add(CollectionModel.fromFirebase(collectionData));
+      }
+
+      return collections;
     } else {
       return null;
     }
