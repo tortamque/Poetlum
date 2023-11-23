2496ea4737d13d54db546014ae07cafc3f59f0d4
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-23 20:18:38 +0200

    Add new cubit support

--- lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
+++ lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
@@ -1,8 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
+import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 
-class SavedPoemsScreen extends StatelessWidget {
+class SavedPoemsScreen extends StatefulWidget {
   const SavedPoemsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Text('Hey, I\'m a placeholder :D');
+  State<SavedPoemsScreen> createState() => _SavedPoemsScreenState();
+}
+
+class _SavedPoemsScreenState extends State<SavedPoemsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initPoems();
+  }
+
+  Future<void> initPoems() async{
+    final poems = await context.read<FirebaseDatabaseCubit>().getUserPoems(FirebaseAuth.instance.currentUser!.uid);
+    print(poems);
+  }
+
+  @override
+  Widget build(BuildContext context) => BlocBuilder<FirebaseDatabaseCubit, FirebaseDatabaseState>(
+    builder: (context, state) {
+      if(state.status == FirebaseDatabaseStatus.submitting){
+        return const CircularProgressIndicator();
+      } else{
+        return Column(
+          children: [
+            TextButton(onPressed: () async{
+              final poems = await context.read<FirebaseDatabaseCubit>().getUserPoems(FirebaseAuth.instance.currentUser!.uid);
+              print(poems);
+            }, child: Text('a')),
+          ],
+        );
+      }
+    },
+  );
 }
