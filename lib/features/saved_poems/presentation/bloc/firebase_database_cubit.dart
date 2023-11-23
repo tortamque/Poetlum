8fac26694d6079408b13549275c47d8400725a01
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-23 21:52:59 +0200

    Extend cubit

--- lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
+++ lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
@@ -1,12 +1,15 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/get_user_collections_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 
 class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
-  FirebaseDatabaseCubit(this._getUserPoemsUseCase) : super(const FirebaseDatabaseState());
+  FirebaseDatabaseCubit(this._getUserPoemsUseCase, this._getUserCollectionsUseCase) : super(const FirebaseDatabaseState());
 
   final GetUserPoemsUseCase _getUserPoemsUseCase;
+  final GetUserCollectionsUseCase _getUserCollectionsUseCase;
 
   Future<List<PoemEntity>?> getUserPoems(String userId) async{
     emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
@@ -18,5 +21,17 @@ class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
     emit(state.copyWith(status: FirebaseDatabaseStatus.success));
 
     return poems;
+  }
+
+  Future<List<CollectionEntity>?> getUserCollections(String userId) async{
+    emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
+
+    final collections = await _getUserCollectionsUseCase(
+      params: userId,
+    );
+
+    emit(state.copyWith(status: FirebaseDatabaseStatus.success));
+
+    return collections;
   }
 }
