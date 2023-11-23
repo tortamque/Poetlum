da83befd54954c18f848c30db9a5cf84c22400f7
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-23 20:18:24 +0200

    Refactor cubit

--- lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
+++ lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
@@ -1,14 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
+import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 
-class FirebaseDatabaseCubit extends Cubit<void> {
-  FirebaseDatabaseCubit(this._getUserPoemsUseCase) : super(null);
+class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
+  FirebaseDatabaseCubit(this._getUserPoemsUseCase) : super(const FirebaseDatabaseState());
 
   final GetUserPoemsUseCase _getUserPoemsUseCase;
 
-  Future<void> getUserPoems(String userId) async{
-    await _getUserPoemsUseCase(
+  Future<List<PoemEntity>?> getUserPoems(String userId) async{
+    emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
+
+    final poems = await _getUserPoemsUseCase(
       params: userId,
     );
+
+    emit(state.copyWith(status: FirebaseDatabaseStatus.success));
+
+    return poems;
   }
 }
