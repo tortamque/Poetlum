6a49d24897a91b43fa2587824aa8a0b9073ea960
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-28 20:13:24 +0200

    Update repository

--- lib/features/saved_poems/domain/repository/firebase_db_repository.dart
+++ lib/features/saved_poems/domain/repository/firebase_db_repository.dart
@@ -4,4 +4,5 @@ import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 abstract class FirebaseDatabaseRepository {
   Future<List<PoemEntity>?> getUserPoems(String userId);
   Future<List<CollectionEntity>?> getUserCollections(String userId);
+  Future<void> saveCustomPoem({required String userId, required PoemEntity poemEntity});
 }
