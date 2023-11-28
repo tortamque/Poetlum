cef53e41c935755cf86a6c75470030c973e8b394
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-28 16:04:45 +0200

    Rewrite code

--- lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
+++ lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/saved_poems/data/models/collection.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_collections_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
@@ -29,6 +30,12 @@ class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
     final collections = await _getUserCollectionsUseCase(
       params: userId,
     );
+
+    final poems = await _getUserPoemsUseCase(
+      params: userId,
+    );
+
+    collections?.insert(0, CollectionModel(name: 'All saved poems', poems: poems));
 
     emit(state.copyWith(status: FirebaseDatabaseStatus.success));
 
--- lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
+++ lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/poems_feed/domain/repository/user_repository.dart';
-import 'package:poetlum/features/saved_poems/data/models/collection.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
@@ -18,31 +16,24 @@ class SavedPoemsScreen extends StatefulWidget {
 }
 
 class _SavedPoemsScreenState extends State<SavedPoemsScreen> {
-  late List<PoemEntity>? poems;
-  late List<CollectionEntity>? collections;
+  List<CollectionEntity>? collections = [];
 
   @override
   void initState() {
     super.initState();
-    initPoems();
     initCollections();
   }
 
-  Future<void> initPoems() async {
-    poems = await context.read<FirebaseDatabaseCubit>().getUserPoems(widget._userRepository.getCurrentUser().userId!);
-  }
 
   Future<void> initCollections() async {
     collections = await context.read<FirebaseDatabaseCubit>().getUserCollections(widget._userRepository.getCurrentUser().userId!);
-
-    collections?.insert(0, CollectionModel(name: 'All saved poems', poems: poems));
   }
 
   @override
   Widget build(BuildContext context) => BlocBuilder<FirebaseDatabaseCubit, FirebaseDatabaseState>(
     builder: (context, state) {
       if (state.status == FirebaseDatabaseStatus.submitting) {
-        return const CircularProgressIndicator();
+        return const Center(child: CircularProgressIndicator());
       } else {
         return SingleChildScrollView(
           child: Column(
@@ -72,6 +63,5 @@ class _SavedPoemsScreenState extends State<SavedPoemsScreen> {
       }
     },
   );
-
 }
 
