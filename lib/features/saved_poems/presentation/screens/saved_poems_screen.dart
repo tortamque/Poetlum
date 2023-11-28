30b360e11018de674c6a1983fb8e2b91f9bb4cb3
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-28 15:47:21 +0200

    Create saved poems screen

--- lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
+++ lib/features/saved_poems/presentation/screens/saved_poems_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/poems_feed/domain/repository/user_repository.dart';
+import 'package:poetlum/features/saved_poems/data/models/collection.dart';
+import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
+import 'package:poetlum/features/saved_poems/presentation/widgets/collections_card.dart';
 
 class SavedPoemsScreen extends StatefulWidget {
   const SavedPoemsScreen(this._userRepository, {super.key});
@@ -14,6 +18,9 @@ class SavedPoemsScreen extends StatefulWidget {
 }
 
 class _SavedPoemsScreenState extends State<SavedPoemsScreen> {
+  late List<PoemEntity>? poems;
+  late List<CollectionEntity>? collections;
+
   @override
   void initState() {
     super.initState();
@@ -21,31 +28,41 @@ class _SavedPoemsScreenState extends State<SavedPoemsScreen> {
     initCollections();
   }
 
-  Future<void> initPoems() async{
-    final poems = await context.read<FirebaseDatabaseCubit>().getUserPoems(widget._userRepository.getCurrentUser().userId!);
-    print(poems);
+  Future<void> initPoems() async {
+    poems = await context.read<FirebaseDatabaseCubit>().getUserPoems(widget._userRepository.getCurrentUser().userId!);
   }
 
-  Future<void> initCollections() async{
-    final collections = await context.read<FirebaseDatabaseCubit>().getUserCollections(widget._userRepository.getCurrentUser().userId!);
-    print(collections);
+  Future<void> initCollections() async {
+    collections = await context.read<FirebaseDatabaseCubit>().getUserCollections(widget._userRepository.getCurrentUser().userId!);
+
+    collections?.insert(0, CollectionModel(name: 'All saved poems', poems: poems));
   }
 
   @override
   Widget build(BuildContext context) => BlocBuilder<FirebaseDatabaseCubit, FirebaseDatabaseState>(
     builder: (context, state) {
-      if(state.status == FirebaseDatabaseStatus.submitting){
+      if (state.status == FirebaseDatabaseStatus.submitting) {
         return const CircularProgressIndicator();
-      } else{
+      } else {
         return Column(
           children: [
-            TextButton(onPressed: () async{
+            TextButton(onPressed: () async {
               final poems = await context.read<FirebaseDatabaseCubit>().getUserCollections(widget._userRepository.getCurrentUser().userId!);
               print(poems);
-            }, child: Text('a')),
+            }, child: const Text('a')),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: collections!.length, 
+                itemBuilder: (context, index) => CollectionsCard(
+                  collection: collections![index],
+                ),
+              ),
+            ),
           ],
         );
       }
     },
   );
 }
+
---
+++ lib/features/saved_poems/presentation/widgets/collections_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
+
+class CollectionsCard extends StatelessWidget {
+  const CollectionsCard({super.key, required this.collection});
+
+  final CollectionEntity collection;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    //onTap: () => Navigator.pushNamed(context, poemViewPageConstant, arguments: poemEntity),
+    child: SizedBox(
+      height: MediaQuery.of(context).size.height / 4,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+        elevation: 3,
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _TitleText(title: collection.name),
+              
+              SingleChildScrollView(
+                child: Column(
+                  children: collection.poems!.map(
+                    (poem) => _InfoText(author: poem.author, title: poem.title),
+                  ).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class _TitleText extends StatelessWidget {
+  const _TitleText({required this.title});
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) => Text(
+      title ?? 'Untitled',
+      style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    );
+}
+
+class _InfoText extends StatelessWidget {
+  const _InfoText({required this.author, required this.title});
+
+  final String? author;
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) => Text(
+      '$author $title',
+      style: const TextStyle(fontSize: 17),
+    );
+}
