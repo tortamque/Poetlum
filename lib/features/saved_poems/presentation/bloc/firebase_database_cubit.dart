29a72352d7ba458428f0d074d23251bcbee4c1eb
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-29 15:17:34 +0200

    Add delePoem in the cubit

--- lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
+++ lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
@@ -2,6 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/saved_poems/data/models/collection.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/delete_poem/delete_poem_params.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/delete_poem/delete_poem_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_collections_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/save_poem/save_poem_params.dart';
@@ -9,11 +11,12 @@ import 'package:poetlum/features/saved_poems/domain/usecases/save_poem/save_poem
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 
 class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
-  FirebaseDatabaseCubit(this._getUserPoemsUseCase, this._getUserCollectionsUseCase, this._savePoemUseCase) : super(const FirebaseDatabaseState());
+  FirebaseDatabaseCubit(this._getUserPoemsUseCase, this._getUserCollectionsUseCase, this._savePoemUseCase, this._deletePoemUseCase) : super(const FirebaseDatabaseState());
 
   final GetUserPoemsUseCase _getUserPoemsUseCase;
   final GetUserCollectionsUseCase _getUserCollectionsUseCase;
   final SavePoemUseCase _savePoemUseCase;
+  final DeletePoemUseCase _deletePoemUseCase;
 
   Future<List<PoemEntity>?> getUserPoems(String userId) async{
     emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
@@ -62,6 +65,24 @@ class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
             text: text,
             title: title,
           ),
+        ),
+      );
+
+      emit(state.copyWith(status: FirebaseDatabaseStatus.success));
+    } catch(e) {
+      emit(state.copyWith(status: FirebaseDatabaseStatus.error));
+    }
+  }
+
+  Future<void> deletePoem({required PoemEntity poemEntity, required String userId, String? collectionName}) async{
+    emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
+
+    try{
+      await _deletePoemUseCase(
+        params: DeletePoemParams(
+          userId: userId,
+          collectionName: collectionName,
+          poemEntity: poemEntity,
         ),
       );
 
