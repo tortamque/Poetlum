95ad1a693e6447faee04f6c7fd4f78714688a1a0
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-29 15:50:53 +0200

    Add isPoemExists in the cubit

--- lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
+++ lib/features/saved_poems/presentation/bloc/firebase_database_cubit.dart
@@ -6,17 +6,20 @@ import 'package:poetlum/features/saved_poems/domain/usecases/delete_poem/delete_
 import 'package:poetlum/features/saved_poems/domain/usecases/delete_poem/delete_poem_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_collections_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/is_poem_exists/is_poem_exists_params.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/is_poem_exists/is_poem_exists_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/save_poem/save_poem_params.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/save_poem/save_poem_usecase.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 
 class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
-  FirebaseDatabaseCubit(this._getUserPoemsUseCase, this._getUserCollectionsUseCase, this._savePoemUseCase, this._deletePoemUseCase) : super(const FirebaseDatabaseState());
+  FirebaseDatabaseCubit(this._getUserPoemsUseCase, this._getUserCollectionsUseCase, this._savePoemUseCase, this._deletePoemUseCase, this._isPoemExistsUseCase) : super(const FirebaseDatabaseState());
 
   final GetUserPoemsUseCase _getUserPoemsUseCase;
   final GetUserCollectionsUseCase _getUserCollectionsUseCase;
   final SavePoemUseCase _savePoemUseCase;
   final DeletePoemUseCase _deletePoemUseCase;
+  final IsPoemExistsUseCase _isPoemExistsUseCase;
 
   Future<List<PoemEntity>?> getUserPoems(String userId) async{
     emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
@@ -89,6 +92,24 @@ class FirebaseDatabaseCubit extends Cubit<FirebaseDatabaseState> {
       emit(state.copyWith(status: FirebaseDatabaseStatus.success));
     } catch(e) {
       emit(state.copyWith(status: FirebaseDatabaseStatus.error));
+    }
+  }
+
+  Future<bool> isPoemExists({required PoemEntity poemEntity, required String userId, String? collectionName}) async{
+    emit(state.copyWith(status: FirebaseDatabaseStatus.submitting));
+
+    try{
+      final isExists = await _isPoemExistsUseCase(
+        params: IsPoemExistsParams(poemEntity: poemEntity, userId: userId),
+      );
+
+      emit(state.copyWith(status: FirebaseDatabaseStatus.success));
+
+      return isExists;
+    } catch(e) {
+      emit(state.copyWith(status: FirebaseDatabaseStatus.error));
+
+      return false;
     }
   }
 }
