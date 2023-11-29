83d24e4311cbaf58e931a742da2a9ddfcfbb413a
tortamque <90132962+tortamque@users.noreply.github.com>
2023-11-29 15:51:22 +0200

    Add isExists functionality to the button

--- lib/features/poems_feed/presentation/widgets/poem_view/custom_like_button.dart
+++ lib/features/poems_feed/presentation/widgets/poem_view/custom_like_button.dart
@@ -12,30 +12,40 @@ class CustomLikeButton extends StatelessWidget {
   final PoemEntity poemEntity;
 
   @override
-  Widget build(BuildContext context) => LikeButton(
-    size: 42,
-    circleColor: CircleColor(start: Theme.of(context).colorScheme.primaryContainer, end: Theme.of(context).colorScheme.primary),
-    bubblesColor: BubblesColor(dotPrimaryColor: Theme.of(context).colorScheme.primaryContainer, dotSecondaryColor: Theme.of(context).colorScheme.primary),
-    onTap: (isLiked) async {
-      if(isLiked == false){
-        await context.read<FirebaseDatabaseCubit>().savePoem(
-          userId: getIt<UserRepository>().getCurrentUser().userId!, 
-          username: poemEntity.author ?? '', 
-          title: poemEntity.title ?? '', 
-          text: poemEntity.text ?? '',
-        );
+  Widget build(BuildContext context) => FutureBuilder(
+    future: context.read<FirebaseDatabaseCubit>().isPoemExists(poemEntity: poemEntity, userId: getIt<UserRepository>().getCurrentUser().userId!),
+    builder: (context, snapshot) {
+      if(snapshot.connectionState == ConnectionState.waiting){
+        return const CircularProgressIndicator();
       } else{
-        await context.read<FirebaseDatabaseCubit>().deletePoem(
-          poemEntity: poemEntity, 
-          userId: getIt<UserRepository>().getCurrentUser().userId!, 
+        return LikeButton(
+          isLiked: snapshot.data,
+          size: 42,
+          circleColor: CircleColor(start: Theme.of(context).colorScheme.primaryContainer, end: Theme.of(context).colorScheme.primary),
+          bubblesColor: BubblesColor(dotPrimaryColor: Theme.of(context).colorScheme.primaryContainer, dotSecondaryColor: Theme.of(context).colorScheme.primary),
+          onTap: (isLiked) async {
+            if(isLiked == false){
+              await context.read<FirebaseDatabaseCubit>().savePoem(
+                userId: getIt<UserRepository>().getCurrentUser().userId!, 
+                username: poemEntity.author ?? '', 
+                title: poemEntity.title ?? '', 
+                text: poemEntity.text ?? '',
+              );
+            } else{
+              await context.read<FirebaseDatabaseCubit>().deletePoem(
+                poemEntity: poemEntity, 
+                userId: getIt<UserRepository>().getCurrentUser().userId!, 
+              );
+            }
+            return !isLiked;
+          },
+          likeBuilder: (isLiked) => Icon(
+            Icons.bookmark_rounded,
+            color: isLiked ? Theme.of(context).colorScheme.primary : Colors.grey,
+            size: 42,
+          ),
         );
       }
-      return !isLiked;
     },
-    likeBuilder: (isLiked) => Icon(
-      Icons.bookmark_rounded,
-      color: isLiked ? Theme.of(context).colorScheme.primary : Colors.grey,
-      size: 42,
-    ),
   );
 }
