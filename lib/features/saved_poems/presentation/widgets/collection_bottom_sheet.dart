f77c1ac986296df4227e0a5d0e5e0f5dfef2b184
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-01 12:59:14 +0200

    Decompoze large widget

--- lib/features/saved_poems/presentation/widgets/collection_bottom_sheet.dart
+++ lib/features/saved_poems/presentation/widgets/collection_bottom_sheet.dart
@@ -4,8 +4,8 @@ import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/custom_spacer.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/drawer/custom_textfield.dart';
 
-class ColectionBottomSheetContent extends StatelessWidget {
-  ColectionBottomSheetContent({super.key, required this.poems});
+class CollectionBottomSheetContent extends StatelessWidget {
+  CollectionBottomSheetContent({super.key, required this.poems});
 
   final List<PoemEntity>? poems;
   final TextEditingController _collectionNameController = TextEditingController();
@@ -13,40 +13,82 @@ class ColectionBottomSheetContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => SizedBox(
-    height: MediaQuery.of(context).size.height/1.15,
+    height: MediaQuery.of(context).size.height / 1.15,
     width: MediaQuery.of(context).size.width,
     child: Column(
       children: [
         const CustomSpacer(heightFactor: 0.05),
-        const Text(
-          'Create a collection',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
+        const _TitleTextWidget(),
         const CustomSpacer(heightFactor: 0.05),
-        CustomTextField(hintText: 'Collection name', controller: _collectionNameController),
+        _CollectionNameInputWidget(controller: _collectionNameController),
         const CustomSpacer(heightFactor: 0.05),
-        SizedBox(
-          width: MediaQuery.of(context).size.width / 1.35,
-          child: MultiSelectDropDown(
-            borderRadius: 4,
-            borderWidth: 1.5,
-            backgroundColor: Colors.transparent,
-            hint: 'Select a poem to add',
-            controller: _controller,
-            onOptionSelected: (selectedOptions) {},
-            options: poems == null 
-              ? <ValueItem<PoemEntity>>[]
-              : poems!.map(
-                (poem) => ValueItem<PoemEntity>(label: '${poem.author ?? ''}: ${poem.title}', value: poem),
-              ).toList(),
-            dropdownHeight: 300,
-            optionTextStyle: const TextStyle(fontSize: 16),
-            selectedOptionIcon: const Icon(Icons.check_circle),
-          ),
-        ),
+        _PoemSelectionWidget(controller: _controller, poems: poems),
         const CustomSpacer(heightFactor: 0.05),
-        FilledButton.tonal(onPressed: (){}, child: const Padding(padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),child: Text("Create")))
+        const _CreateButtonWidget(),
       ],
     ),
   );
+}
+
+class _TitleTextWidget extends StatelessWidget {
+  const _TitleTextWidget();
+
+  @override
+  Widget build(BuildContext context) => const Text(
+      'Create a collection',
+      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    );
+}
+
+class _CollectionNameInputWidget extends StatelessWidget {
+  const _CollectionNameInputWidget({required this.controller});
+  
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) => CustomTextField(hintText: 'Collection name', controller: controller);
+}
+
+class _PoemSelectionWidget extends StatelessWidget {
+  const _PoemSelectionWidget({required this.controller, this.poems});
+
+  final MultiSelectController<PoemEntity> controller;
+  final List<PoemEntity>? poems;
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+      width: MediaQuery.of(context).size.width / 1.35,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 1.35,
+        child: MultiSelectDropDown(
+          borderRadius: 4,
+          borderWidth: 1.5,
+          backgroundColor: Colors.transparent,
+          hint: 'Select a poem to add',
+          controller: controller,
+          onOptionSelected: (selectedOptions) {},
+          options: poems == null 
+            ? <ValueItem<PoemEntity>>[]
+            : poems!.map(
+              (poem) => ValueItem<PoemEntity>(label: '${poem.author ?? ''}: ${poem.title}', value: poem),
+            ).toList(),
+          dropdownHeight: 300,
+          optionTextStyle: const TextStyle(fontSize: 16),
+          selectedOptionIcon: const Icon(Icons.check_circle),
+        ),
+      ),
+    );
+}
+
+class _CreateButtonWidget extends StatelessWidget {
+  const _CreateButtonWidget();
+
+  @override
+  Widget build(BuildContext context) => FilledButton.tonal(
+      onPressed: () {}, 
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Text('Create')
+      ),
+    );
 }
