63fcaebdb87e8b95d1fa4c3722ace27a990e2dbe
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-01 15:06:46 +0200

    Init DI

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -22,6 +22,7 @@ import 'package:poetlum/features/realtime_database/domain/entities/database_mana
 import 'package:poetlum/features/saved_poems/data/data_sources/remote/firebase_api_service.dart';
 import 'package:poetlum/features/saved_poems/data/repository/firebase_db_repository_impl.dart';
 import 'package:poetlum/features/saved_poems/domain/repository/firebase_db_repository.dart';
+import 'package:poetlum/features/saved_poems/domain/usecases/create_new_collection/create_new_collection_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/delete_poem/delete_poem_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_collections_usecase.dart';
 import 'package:poetlum/features/saved_poems/domain/usecases/get_user_poems_usecase.dart';
@@ -62,6 +63,7 @@ void initializeDependencies() {
       ..registerSingleton<SavePoemUseCase>(SavePoemUseCase(getIt()))
       ..registerSingleton<DeletePoemUseCase>(DeletePoemUseCase(getIt()))
       ..registerSingleton<IsPoemExistsUseCase>(IsPoemExistsUseCase(getIt()))
+      ..registerSingleton<CreateNewCollectionUseCase>(CreateNewCollectionUseCase(getIt()))
 
       // Validators
       ..registerLazySingleton<UsernameValidator>(() => UsernameValidator())
@@ -71,7 +73,7 @@ void initializeDependencies() {
       // Bloc
       ..registerFactory<RemotePoemBloc>(() => RemotePoemBloc(getIt(), getIt()))
       ..registerFactory<AuthCubit>(() => AuthCubit(getIt(), getIt()))
-      ..registerFactory<FirebaseDatabaseCubit>(() => FirebaseDatabaseCubit(getIt(), getIt(), getIt(), getIt(), getIt()))
+      ..registerFactory<FirebaseDatabaseCubit>(() => FirebaseDatabaseCubit(getIt(), getIt(), getIt(), getIt(), getIt(), getIt()))
       ..registerFactory<RegisterFormValidationCubit>(() => RegisterFormValidationCubit(
         usernameValidator: getIt<UsernameValidator>(),
         emailValidator: getIt<LocalEmailValidator>(),
