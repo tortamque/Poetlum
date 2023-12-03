9e38b1fbada54e20f6a07a36a2946d78d6e0574b
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-03 17:41:34 +0200

    Add deleteCollection function

--- lib/features/saved_poems/data/data_sources/remote/firebase_api_service.dart
+++ lib/features/saved_poems/data/data_sources/remote/firebase_api_service.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_dynamic_calls
+// ignore_for_file: avoid_dynamic_calls, cascade_invocations
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:poetlum/features/poems_feed/data/models/poem.dart';
@@ -12,6 +12,7 @@ abstract class FirebaseDatabaseService{
   Future<void> deletePoem({required PoemEntity poemEntity, required String userId, required String? collectionName});
   Future<bool> isPoemExists({required PoemEntity poemEntity, required String userId});
   Future<void> createNewCollection({required String userId, required String collectionName, required List<PoemEntity> poems});
+  Future<void> deleteCollection({required String userId, required String collectionName, required List<PoemEntity> poems});
 }
 
 class FirebaseDatabaseServiceImpl implements FirebaseDatabaseService {
@@ -131,4 +132,50 @@ class FirebaseDatabaseServiceImpl implements FirebaseDatabaseService {
 
     await collectionsRef.push().set(newCollectionData);
   }
+
+  @override
+  Future<void> deleteCollection({required String userId, required String collectionName, required List<PoemEntity> poems}) async {
+    final userRef = FirebaseDatabase.instance.ref(userId);
+    final collectionsRef = userRef.child('collections');
+
+    final collectionsSnapshot = await collectionsRef.get();
+
+    if (!collectionsSnapshot.exists) {
+      return;
+    }
+
+    final collections = collectionsSnapshot.value as Map<dynamic, dynamic>;
+    collections.forEach((key, value) {
+      if (value['name'] == collectionName) {
+        final collectionPoems = (value['poems'] as List<dynamic>)
+            .map((poem) {
+              final poemData = Map<String, dynamic>.from(poem as Map);
+              final poemModel = PoemModel.fromFirebase(poemData);
+              
+              return poemModel;
+            })
+            .toList();
+
+        if (_arePoemListsEqual(collectionPoems, poems)) {
+          collectionsRef.child(key).remove();
+        }
+      }
+    });
+  }
+
+  bool _arePoemListsEqual(List<PoemEntity> list1, List<PoemEntity> list2) {
+    if (list1.length != list2.length) return false;
+
+    for (final poem in list1) {
+      if (!list2.any((p) => _isPoemEqual(p, poem))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool _isPoemEqual(PoemEntity poem1, PoemEntity poem2) => poem1.title == poem2.title &&
+          poem1.author == poem2.author &&
+          poem1.text == poem2.text &&
+          poem1.linecount == poem2.linecount;
 }
