a1f74806419dee321fc23aa9f9c5bc7cb392fe74
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-03 18:12:50 +0200

    Create SavedPoemViewPage template

---
+++ lib/features/saved_poems/presentation/pages/saved_poem_view_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:poetlum/features/application/presentation/widgets/app_bar/app_bar.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/custom_spacer.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/custom_like_button.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_author.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_content.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_line_count.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_title.dart';
+
+class SavedPoemViewPage extends StatelessWidget {
+  const SavedPoemViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final poemEntity = (ModalRoute.of(context)?.settings.arguments ?? const PoemEntity()) as PoemEntity;
+
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Poetlum'),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                const CustomSpacer(heightFactor: 0.02),
+                CustomLikeButton(poemEntity: poemEntity),
+                const CustomSpacer(heightFactor: 0.02),
+                PoemTitle(title: poemEntity.title ?? ''),
+                const CustomSpacer(heightFactor: 0.02),
+                PoemAuthor(author: poemEntity.author ?? ''),
+                const CustomSpacer(heightFactor: 0.02),
+                PoemContent(text: poemEntity.text ?? ''),
+                const CustomSpacer(heightFactor: 0.02),
+                PoemLineCount(lineCount: poemEntity.linecount ?? 0),
+                const CustomSpacer(heightFactor: 0.02),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
