c66517bda72bd99479405d1ff7a21340e73ae17b
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-03 18:46:38 +0200

    Create savedCollectionView Constant

--- lib/core/constants/navigator_constants.dart
+++ lib/core/constants/navigator_constants.dart
@@ -4,4 +4,5 @@ const String loginPageConstant = '/login';
 const String screensWrapperPageConstant = '/screen_wrapper';
 const String poemViewPageConstant = '/poem_view';
 const String writePoemPageConstant = '/write_poem';
+const String savedCollectionViewConstant = '/saved_collection_view';
 const String savedPoemViewConstant = '/saved_poem_view';
