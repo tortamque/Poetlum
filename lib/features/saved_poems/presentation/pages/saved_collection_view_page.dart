152e90a3c60917837e965367ad3926853f697619
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-03 18:47:14 +0200

    Create SavedCollectionViewPage

---
+++ lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:poetlum/features/application/presentation/widgets/app_bar/app_bar.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/poems_feed/poem_card.dart';
+import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
+
+class SavedCollectionViewPage extends StatelessWidget {
+  const SavedCollectionViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    final collectionEntity = (ModalRoute.of(context)?.settings.arguments ?? const CollectionEntity(isAllSavedPoems: false)) as CollectionEntity;
+
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Poetlum'),
+      body: ListView.builder(
+        itemCount: collectionEntity.poems?.length ?? 0,
+        itemBuilder: (__, index) => PoemCard(
+          poemEntity: collectionEntity.poems?[index] ?? const PoemEntity(),
+        ),
+      ),
+    );
+  }
+}
