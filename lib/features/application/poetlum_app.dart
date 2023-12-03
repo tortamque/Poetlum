e9929652f20d60b3a5f02b67b1fa45d0ce0868b4
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-03 18:46:55 +0200

    Register SavedCollectionView Page

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -8,6 +8,7 @@ import 'package:poetlum/features/application/presentation/pages/screens_wrapper.
 import 'package:poetlum/features/authorization/presentation/pages/login/login_page.dart';
 import 'package:poetlum/features/authorization/presentation/pages/registration/registration_page.dart';
 import 'package:poetlum/features/poems_feed/presentation/pages/poem_view/poem_view.dart';
+import 'package:poetlum/features/saved_poems/presentation/pages/saved_collection_view_page.dart';
 import 'package:poetlum/features/saved_poems/presentation/pages/saved_poem_view_page.dart';
 import 'package:poetlum/features/saved_poems/presentation/pages/write_poem_page.dart';
 
@@ -26,6 +27,7 @@ class PoetlumApp extends StatelessWidget {
       screensWrapperPageConstant: (_) => const ScreensWrapper(),
       poemViewPageConstant:(_) => const PoemViewPage(),
       writePoemPageConstant: (_) => WritePoemPage(getIt()),
+      savedCollectionViewConstant: (_) => const SavedCollectionViewPage(),
       savedPoemViewConstant: (_) => const SavedPoemViewPage(),
     },
   );
