bfffa206c7b09ead23cd37a873701d5a66100339
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-06 22:45:25 +0200

    Fetch poems on page loading

--- lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
+++ lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
@@ -6,42 +6,85 @@ import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/poems_feed/domain/repository/user_repository.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
+import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
 import 'package:poetlum/features/saved_poems/presentation/widgets/saved_poem_card.dart';
 import 'package:poetlum/features/saved_poems/presentation/widgets/update_collection_bottom_sheet_content.dart';
 
-class SavedCollectionViewPage extends StatelessWidget {
+class SavedCollectionViewPage extends StatefulWidget {
   const SavedCollectionViewPage({super.key});
 
   @override
+  State<SavedCollectionViewPage> createState() => _SavedCollectionViewPageState();
+}
+
+class _SavedCollectionViewPageState extends State<SavedCollectionViewPage> {
+  List<PoemEntity> poems = [];
+  late CollectionEntity collectionEntity;
+  bool isInit = true;
+  
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (isInit) {
+      collectionEntity = (ModalRoute.of(context)?.settings.arguments ?? const CollectionEntity(isAllSavedPoems: false)) as CollectionEntity;
+      initPoems();
+      isInit = false;
+    }
+  }
+
+  Future<void> initPoems() async {
+    poems = await context.read<FirebaseDatabaseCubit>().getPoemsInCollection(
+      userId: getIt<UserRepository>().getCurrentUser().userId!, 
+      collectionName: collectionEntity.name ?? '',
+    );
+  }
+  
+  @override
   Widget build(BuildContext context){
-    final collectionEntity = (ModalRoute.of(context)?.settings.arguments ?? const CollectionEntity(isAllSavedPoems: false)) as CollectionEntity;
+    final localContext = context;
 
     return Scaffold(
       appBar: const CustomAppBar(title: 'Poetlum'),
       floatingActionButton: FloatingActionButton(
+        tooltip: 'Edit a collection',
         onPressed: () async {
           final savedPoems = await context.read<FirebaseDatabaseCubit>().getUserPoems(
             getIt<UserRepository>().getCurrentUser().userId!,
           );
 
-          await showModalBottomSheet(
-            context: context, 
-            isScrollControlled: true,
-            builder:(context) => UpdateCollectionBottomSheetContent(
-              collectionName: collectionEntity.name ?? '',
-              poemsInTheCollection: collectionEntity.poems,
-              allSavedPoems: savedPoems,
-            ),
-          );
+          if (mounted){
+            await showModalBottomSheet(
+              context: localContext, 
+              isScrollControlled: true,
+              builder:(context) => UpdateCollectionBottomSheetContent(
+                collectionName: collectionEntity.name ?? '',
+                poemsInTheCollection: collectionEntity.poems,
+                allSavedPoems: savedPoems,
+              ),
+            );
+          }
         },
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.edit),
       ),
-      body: ListView.builder(
-        itemCount: collectionEntity.poems?.length ?? 0,
-        itemBuilder: (__, index) => SavedPoemCard(
-          poemEntity: collectionEntity.poems?[index] ?? const PoemEntity(),
-          collectionEntity: collectionEntity,
-        ),
+      body: BlocConsumer<FirebaseDatabaseCubit, FirebaseDatabaseState>(
+        listener: (context, state) {
+          if (state.status == FirebaseDatabaseStatus.needsRefresh) {
+            initPoems();
+          }
+        },
+        builder: (context, state) {
+          if (state.status == FirebaseDatabaseStatus.submitting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return  ListView.builder(
+              itemCount: poems.length,
+              itemBuilder: (__, index) => SavedPoemCard(
+                poemEntity: poems[index],
+                collectionEntity: collectionEntity,
+              ),
+            );
+          }
+        },
       ),
     );
   }
