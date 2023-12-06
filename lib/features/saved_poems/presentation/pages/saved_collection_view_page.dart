3ae3d5b6d841ccc34161a8a33d24e9637a0248d0
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-06 17:15:13 +0200

    Create FloatingActionButton template

--- lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
+++ lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poetlum/core/dependency_injection.dart';
 import 'package:poetlum/features/application/presentation/widgets/app_bar/app_bar.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/poems_feed/domain/repository/user_repository.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
+import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
+import 'package:poetlum/features/saved_poems/presentation/widgets/collection_bottom_sheet.dart';
 import 'package:poetlum/features/saved_poems/presentation/widgets/saved_poem_card.dart';
 
 class SavedCollectionViewPage extends StatelessWidget {
@@ -13,6 +18,22 @@ class SavedCollectionViewPage extends StatelessWidget {
 
     return Scaffold(
       appBar: const CustomAppBar(title: 'Poetlum'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showModalBottomSheet(
+            context: context, 
+            isScrollControlled: true,
+            builder:(context) => CollectionBottomSheetContent(
+              poems: collectionEntity.poems,
+            ),
+          );
+
+          final savedPoems = await context.read<FirebaseDatabaseCubit>().getUserPoems(
+            getIt<UserRepository>().getCurrentUser().userId!,
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
       body: ListView.builder(
         itemCount: collectionEntity.poems?.length ?? 0,
         itemBuilder: (__, index) => SavedPoemCard(
