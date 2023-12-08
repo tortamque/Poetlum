d8202aa7a339b4caa5973206fde2d0c8e8fb2972
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-08 17:36:57 +0200

    Init theme on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:poetlum/features/dependency_injection/presentation/widgets/init_
 import 'package:poetlum/features/firebase/presentation/widgets/init_crashlytics_widget.dart';
 import 'package:poetlum/features/firebase/presentation/widgets/init_firebase_widget.dart';
 import 'package:poetlum/features/multi_bloc_provider/presentation/init_blocs.dart';
+import 'package:poetlum/features/theme_change/presentation/widgets/init_theme_widget.dart';
 import 'package:poetlum/firebase_options.dart';
 
 void main() async {
@@ -15,7 +16,9 @@ void main() async {
       child: const InitDependencies(
         child: InitCrashlyticsWidget(
           child: InitBlocs(
-            child: PoetlumApp(),
+            child: InitTheme(
+              child: PoetlumApp(),
+            ),
           ),
         ),
       ),
