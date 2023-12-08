a525689eff550a3057f8d47c349d5ea1646b7481
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-08 16:25:58 +0200

    Implement cubit

--- lib/features/application/poetlum_app.dart
+++ lib/features/application/poetlum_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:poetlum/config/theme/app_theme.dart';
 import 'package:poetlum/core/constants/navigator_constants.dart';
 import 'package:poetlum/core/dependency_injection.dart';
 import 'package:poetlum/features/application/presentation/pages/auth_wrapper.dart';
@@ -11,24 +11,28 @@ import 'package:poetlum/features/poems_feed/presentation/pages/poem_view/poem_vi
 import 'package:poetlum/features/saved_poems/presentation/pages/saved_collection_view_page.dart';
 import 'package:poetlum/features/saved_poems/presentation/pages/saved_poem_view_page.dart';
 import 'package:poetlum/features/saved_poems/presentation/pages/write_poem_page.dart';
+import 'package:poetlum/features/theme_change/presentation/bloc/change_theme_cubit.dart';
+import 'package:poetlum/features/theme_change/presentation/bloc/change_theme_state.dart';
 
 class PoetlumApp extends StatelessWidget {
   const PoetlumApp({super.key});
 
   @override
-  Widget build(BuildContext context) => GetMaterialApp(
-    title: 'Poetlum',
-    theme: theme(),
-    initialRoute: authWrapperPageConstant,
-    routes: {
-      authWrapperPageConstant:(_) => const AuthWrapper(),
-      registerPageConstant: (_) => const RegistrationPage(),
-      loginPageConstant: (_) => const LoginPage(),
-      screensWrapperPageConstant: (_) => const ScreensWrapper(),
-      poemViewPageConstant:(_) => const PoemViewPage(),
-      writePoemPageConstant: (_) => WritePoemPage(getIt()),
-      savedCollectionViewConstant: (_) => const SavedCollectionViewPage(),
-      savedPoemViewConstant: (_) => const SavedPoemViewPage(),
-    },
+  Widget build(BuildContext context) => BlocBuilder<ThemeCubit, ThemeState>(
+    builder: (context, state) => GetMaterialApp(
+      title: 'Poetlum',
+      theme: state.themeData,
+      initialRoute: authWrapperPageConstant,
+      routes: {
+        authWrapperPageConstant:(_) => const AuthWrapper(),
+        registerPageConstant: (_) => const RegistrationPage(),
+        loginPageConstant: (_) => const LoginPage(),
+        screensWrapperPageConstant: (_) => const ScreensWrapper(),
+        poemViewPageConstant:(_) => const PoemViewPage(),
+        writePoemPageConstant: (_) => WritePoemPage(getIt()),
+        savedCollectionViewConstant: (_) => const SavedCollectionViewPage(),
+        savedPoemViewConstant: (_) => const SavedPoemViewPage(),
+      },
+    ),
   );
 }
