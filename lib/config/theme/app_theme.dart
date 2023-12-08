d59724043ed14d9d060c4a7acf5c981856ff5d36
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-08 16:46:03 +0200

    Remove unused field

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,5 +3,4 @@ import 'package:flutter/material.dart';
 ThemeData theme(Color color) => ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: color),
   useMaterial3: true,
-  scaffoldBackgroundColor: Colors.white,
 );
