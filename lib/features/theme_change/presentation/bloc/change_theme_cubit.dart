6ce81eb12e9948c1e935883f98b701af406ceae3
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-08 17:15:01 +0200

    Update cubit

--- lib/features/theme_change/presentation/bloc/change_theme_cubit.dart
+++ lib/features/theme_change/presentation/bloc/change_theme_cubit.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/config/theme/app_theme.dart';
+import 'package:poetlum/features/theme_change/domain/usecases/get_color/get_color_usecase.dart';
+import 'package:poetlum/features/theme_change/domain/usecases/save_color/save_color_usecase.dart';
 import 'package:poetlum/features/theme_change/presentation/bloc/change_theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeState.initial());
+  ThemeCubit(this._saveColorUseCase, this._getColorUseCase) : super(ThemeState.initial());
 
-  void setThemeColor(Color themeColor) {
+  final SaveColorUseCase _saveColorUseCase;
+  final GetColorUseCase _getColorUseCase;
+
+  Future<void> setThemeColor(Color themeColor) async {
     emit(
       ThemeState(
         themeData: theme(themeColor),
       ),
     );
+
+    await _saveColorUseCase(params: themeColor);
   }
+
+  Future<Color> getThemeColor() async => _getColorUseCase();
 }
