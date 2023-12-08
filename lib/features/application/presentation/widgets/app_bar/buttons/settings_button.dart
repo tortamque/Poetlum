12d8c9c503804d649c7763de636bdbe72bed884c
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-08 16:25:30 +0200

    Add bottom sheet on button tap

--- lib/features/application/presentation/widgets/app_bar/buttons/settings_button.dart
+++ lib/features/application/presentation/widgets/app_bar/buttons/settings_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:poetlum/features/application/presentation/widgets/app_bar/buttons/rotating_button_mixin.dart';
+import 'package:poetlum/features/theme_change/presentation/widgets/color_options.dart';
 
 class SettingsButton extends StatefulWidget {
   const SettingsButton({super.key});
@@ -14,9 +15,13 @@ class _SettingsButtonState extends State<SettingsButton> with TickerProviderStat
     turns: rotationAnimation,
     child: IconButton(
       tooltip: 'Settings',
-      onPressed: (){
+      onPressed: () async{
         playAnimation();
-        // ...
+        
+        await showModalBottomSheet(
+          context: context, 
+          builder: (context) => _buildBottomSheetContent(),
+        );
       },
       icon: const Icon(Icons.settings),
     ),
@@ -27,4 +32,18 @@ class _SettingsButtonState extends State<SettingsButton> with TickerProviderStat
     rotationController.dispose();
     super.dispose();
   }
+
+  Widget _buildBottomSheetContent() => SizedBox(
+    child: Column(
+      children: [
+        const Text('Choose your theme'),
+        Expanded( 
+          child: GridView.count(
+            crossAxisCount: 3,
+            children: ColorOptions.colors,
+          ),
+        ),
+      ],
+    ),
+  );
 }
