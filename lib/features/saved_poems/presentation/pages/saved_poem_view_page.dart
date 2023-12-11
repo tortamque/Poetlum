afb8fe72bd282485dab6cc280b349237a0f52648
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-11 21:29:15 +0200

    Add animations in the SavedPoemViewPage

--- lib/features/saved_poems/presentation/pages/saved_poem_view_page.dart
+++ lib/features/saved_poems/presentation/pages/saved_poem_view_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:poetlum/features/application/presentation/widgets/app_bar/app_bar.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/animations/top_animation.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/custom_spacer.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/custom_share_button.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_author.dart';
@@ -8,8 +9,46 @@ import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_
 import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_line_count.dart';
 import 'package:poetlum/features/poems_feed/presentation/widgets/poem_view/poem_title.dart';
 
-class SavedPoemViewPage extends StatelessWidget {
+class SavedPoemViewPage extends StatefulWidget {
   const SavedPoemViewPage({super.key});
+
+  @override
+  State<SavedPoemViewPage> createState() => _SavedPoemViewPageState();
+}
+
+class _SavedPoemViewPageState extends State<SavedPoemViewPage> {
+  bool isTitleAnimated = false;
+  bool isAuthorAnimated = false;
+  bool isShareButtonAnimated = false;
+  bool isContentAnimated = false;
+  bool isPoemLineAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
+
+  void _startAnimations() {
+    final setters = <Function(bool)>[
+      (val) => isTitleAnimated = val,
+      (val) => isAuthorAnimated = val,
+      (val) => isShareButtonAnimated = val,
+      (val) => isContentAnimated = val,
+      (val) => isPoemLineAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      Future.delayed(animationDelay * (i + 1)).then(
+        (_){
+          if (mounted) {
+            setState(() => setters[i](true));
+          }
+        }
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,17 +63,46 @@ class SavedPoemViewPage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 24),
             child: Column(
               children: [
+                const CustomSpacer(heightFactor: 0.04),
+
+                TopAnimation(
+                  animationField: isTitleAnimated,
+                  positionInitialValue: MediaQuery.of(context).size.width/8,
+                  opacityInitialValue: 0,
+                  child: PoemTitle(title: poemEntity.title ?? ''),
+                ),
                 const CustomSpacer(heightFactor: 0.02),
+
+                TopAnimation(
+                  animationField: isAuthorAnimated,
+                  positionInitialValue: MediaQuery.of(context).size.width/8,
+                  opacityInitialValue: 0,
+                  child: PoemAuthor(author: poemEntity.author ?? ''),
+                ),
                 const CustomSpacer(heightFactor: 0.02),
-                PoemTitle(title: poemEntity.title ?? ''),
+
+                TopAnimation(
+                  animationField: isShareButtonAnimated,
+                  positionInitialValue: MediaQuery.of(context).size.width/8,
+                  opacityInitialValue: 0,
+                  child: CustomShareButton(poemEntity: poemEntity),
+                ),
                 const CustomSpacer(heightFactor: 0.02),
-                PoemAuthor(author: poemEntity.author ?? ''),
+
+                TopAnimation(
+                  animationField: isContentAnimated,
+                  positionInitialValue: MediaQuery.of(context).size.width/8,
+                  opacityInitialValue: 0,
+                  child: PoemContent(text: poemEntity.text ?? ''),
+                ),
                 const CustomSpacer(heightFactor: 0.02),
-                CustomShareButton(poemEntity: poemEntity),
-                const CustomSpacer(heightFactor: 0.02),
-                PoemContent(text: poemEntity.text ?? ''),
-                const CustomSpacer(heightFactor: 0.02),
-                PoemLineCount(lineCount: poemEntity.linecount ?? 0),
+
+                TopAnimation(
+                  animationField: isPoemLineAnimated,
+                  positionInitialValue: MediaQuery.of(context).size.width/8,
+                  opacityInitialValue: 0,
+                  child: PoemLineCount(lineCount: poemEntity.linecount ?? 0),
+                ),
                 const CustomSpacer(heightFactor: 0.02),
               ],
             ),
