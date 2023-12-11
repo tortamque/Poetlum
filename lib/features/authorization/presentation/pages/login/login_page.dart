55324b4db3d3a59c2fa74d9a752e33918726e015
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-11 23:16:43 +0200

    Add animations to the login page

--- lib/features/authorization/presentation/pages/login/login_page.dart
+++ lib/features/authorization/presentation/pages/login/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poetlum/core/constants/navigator_constants.dart';
@@ -8,62 +10,147 @@ import 'package:poetlum/features/authorization/presentation/bloc/validation/vali
 import 'package:poetlum/features/authorization/presentation/widgets/auth_button.dart';
 import 'package:poetlum/features/authorization/presentation/widgets/email_field.dart';
 import 'package:poetlum/features/authorization/presentation/widgets/password_field.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/animations/right_animation.dart';
 
-class _Header extends StatelessWidget {
+class _Header extends StatefulWidget {
   const _Header();
+
+  @override
+  State<_Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<_Header> {
+  bool isHeaderAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+  void _startAnimations() {
+    final setters = <Function(bool)>[
+      (val) => isHeaderAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      Future.delayed(animationDelay * (i + 1)).then(
+        (_) => setState(() => setters[i](true)),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
 
   @override
   Widget build(BuildContext context) => SizedBox(
     height: MediaQuery.of(context).size.height/10,
-    child: const Column(
+    child: Column(
       children: [
-        Spacer(),
-        Text(
-          'Login',
-          style: TextStyle(
-            fontSize: 22,
+        const Spacer(),
+
+        RightAnimation(
+          animationField: isHeaderAnimated,
+          positionInitialValue: MediaQuery.of(context).size.height/14,
+          opacityInitialValue: 0,
+          child: const Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 22,
+            ),
           ),
         ),
-        Spacer(),
+        
+        const Spacer(),
       ],
     ),
   );
 }
 
-class _Form extends StatelessWidget {
+class _Form extends StatefulWidget {
   const _Form(this.emailController, this.passwordController);
 
   final TextEditingController emailController;
   final TextEditingController passwordController;
 
   @override
+  State<_Form> createState() => _FormState();
+}
+
+class _FormState extends State<_Form> {
+  bool isUsernameAnimated = false;
+  bool isEmailAnimated = false;
+  bool isPasswordAnimated = false;
+  bool isButtonAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+
+  Future<void> _startAnimations() async {
+    await Future.delayed(const Duration(milliseconds: 200));
+
+    final setters = <Function(bool)>[
+      (val) => isEmailAnimated = val,
+      (val) => isPasswordAnimated = val,
+      (val) => isButtonAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      unawaited(
+        Future.delayed(animationDelay * (i + 1)).then(
+          (_) => setState(() => setters[i](true)),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
+  
+  @override
   Widget build(BuildContext context) => BlocBuilder<LoginFormValidationCubit, LoginFormValidationState>(
     builder: (context, state) => SizedBox(
       height: MediaQuery.of(context).size.height/3.5,
       child: Column(
         children: [
-          EmailTextField<LoginFormValidationCubit, LoginFormValidationState>(controller: emailController),
+          RightAnimation(
+            animationField: isEmailAnimated,
+            positionInitialValue: MediaQuery.of(context).size.height/14,
+            opacityInitialValue: 0,
+            child: EmailTextField<LoginFormValidationCubit, LoginFormValidationState>(controller: widget.emailController),
+          ),
           const Spacer(),
-        
-          PasswordTextField<LoginFormValidationCubit, LoginFormValidationState>(controller: passwordController),
+
+          RightAnimation(
+            animationField: isPasswordAnimated,
+            positionInitialValue: MediaQuery.of(context).size.height/14,
+            opacityInitialValue: 0,
+            child: PasswordTextField<LoginFormValidationCubit, LoginFormValidationState>(controller: widget.passwordController),
+          ),
           const Spacer(),
-        
-          AuthButton<
-            AuthCubit, 
-            AuthState, 
-            LoginFormValidationCubit, 
-            LoginFormValidationState
-          >(
-            isEnabled: state.isFormValid,
-            text: 'Login',
-            successfulToastText: 'Your login was successful',
-            onPressed: () {
-              context.read<AuthCubit>().login(
-                email: state.emailValidationState.value,
-                password: state.passwordValidationState.value,
-              );
-            },
-            navigateOnSuccess: () => Navigator.pushNamedAndRemoveUntil(context, screensWrapperPageConstant, (route) => false),
+
+          RightAnimation(
+            animationField: isButtonAnimated,
+            positionInitialValue: MediaQuery.of(context).size.height/14,
+            opacityInitialValue: 0,
+            child: AuthButton<
+              AuthCubit, 
+              AuthState, 
+              LoginFormValidationCubit, 
+              LoginFormValidationState
+            >(
+              isEnabled: state.isFormValid,
+              text: 'Login',
+              successfulToastText: 'Your login was successful',
+              onPressed: () {
+                context.read<AuthCubit>().login(
+                  email: state.emailValidationState.value,
+                  password: state.passwordValidationState.value,
+                );
+              },
+              navigateOnSuccess: () => Navigator.pushNamedAndRemoveUntil(context, screensWrapperPageConstant, (route) => false),
+            ),
           ),
         ],
       ),
@@ -71,26 +158,61 @@ class _Form extends StatelessWidget {
   );
 }
 
-class _Footer extends StatelessWidget {
+class _Footer extends StatefulWidget {
   const _Footer();
+
+  @override
+  State<_Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<_Footer> {
+  bool isFooterAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+  Future<void> _startAnimations() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+
+    final setters = <Function(bool)>[
+      (val) => isFooterAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      unawaited(
+        Future.delayed(animationDelay * (i + 1)).then(
+          (_) => setState(() => setters[i](true)),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
 
   @override
   Widget build(BuildContext context) => SizedBox(
     height: MediaQuery.of(context).size.height/10,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text("Don't have an account?"),
-        TextButton(
-          onPressed: (){
-            Navigator.pushNamedAndRemoveUntil(context, registerPageConstant, (r) => false);
-          }, 
-          child: const Text(
-            'Register', 
-            style: TextStyle(decoration: TextDecoration.underline),
+    child: RightAnimation(
+      animationField: isFooterAnimated,
+      positionInitialValue: MediaQuery.of(context).size.height/14,
+      opacityInitialValue: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Don't have an account?"),
+          TextButton(
+            onPressed: (){
+              Navigator.pushNamedAndRemoveUntil(context, registerPageConstant, (r) => false);
+            }, 
+            child: const Text(
+              'Register', 
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
