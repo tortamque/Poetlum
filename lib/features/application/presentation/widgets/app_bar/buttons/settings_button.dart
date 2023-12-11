5bd9d85d0c30e57be5f5f07de42fe461993dbc49
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-11 19:17:23 +0200

    Add animation to the color options

--- lib/features/application/presentation/widgets/app_bar/buttons/settings_button.dart
+++ lib/features/application/presentation/widgets/app_bar/buttons/settings_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:poetlum/features/application/presentation/widgets/app_bar/buttons/rotating_button_mixin.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/animations/right_animation.dart';
+import 'package:poetlum/features/theme_change/presentation/widgets/animated_color_option_button.dart';
 import 'package:poetlum/features/theme_change/presentation/widgets/color_options.dart';
 
 class SettingsButton extends StatefulWidget {
@@ -20,7 +22,7 @@ class _SettingsButtonState extends State<SettingsButton> with TickerProviderStat
         
         await showModalBottomSheet(
           context: context, 
-          builder: (context) => _buildBottomSheetContent(),
+          builder: (context) => _BottomSheetContent(),
         );
       },
       icon: const Icon(Icons.settings),
@@ -32,20 +34,6 @@ class _SettingsButtonState extends State<SettingsButton> with TickerProviderStat
     rotationController.dispose();
     super.dispose();
   }
-
-  Widget _buildBottomSheetContent() => SingleChildScrollView(
-    child: Column(
-      children: [
-        const _Title(text: 'Choose your theme'),
-        GridView.count(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(), 
-          crossAxisCount: 3,
-          children: ColorOptions.colors,
-        ),
-      ],
-    ),
-  );
 }
 
 class _Title extends StatelessWidget {
@@ -62,6 +50,64 @@ class _Title extends StatelessWidget {
         fontSize: 24,
         fontWeight: FontWeight.bold,
       ),
+    ),
+  );
+}
+
+class _BottomSheetContent extends StatefulWidget {
+  const _BottomSheetContent();
+
+  @override
+  State<_BottomSheetContent> createState() => _BottomSheetContentState();
+}
+
+class _BottomSheetContentState extends State<_BottomSheetContent> {
+  bool isHeaderAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
+
+  void _startAnimations() {
+    final setters = <Function(bool)>[
+      (val) => isHeaderAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      Future.delayed(animationDelay * (i + 1)).then(
+        (_) => setState(() => setters[i](true)),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+    child: Column(
+      children: [
+        RightAnimation(
+          animationField: isHeaderAnimated,
+          positionInitialValue: MediaQuery.of(context).size.width/6,
+          opacityInitialValue: 0,
+          child: const _Title(text: 'Choose your theme'),
+        ),
+
+        GridView.count(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          crossAxisCount: 3,
+          children: List.generate(
+            ColorOptions.colors.length,
+            (index) => AnimatedColorOptionButton(
+              button: ColorOptions.colors[index],
+              delay: Duration(milliseconds: animationDelay.inMilliseconds * index),
+              positionInitialValue: MediaQuery.of(context).size.height/15,
+            ),
+          ),
+        ),
+      ],
     ),
   );
 }
