f0fa7a2e18e3cae701aca4585831661ffb19016d
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-12 11:03:41 +0200

    Fix internet connection monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:poetlum/features/application/poetlum_app.dart';
 import 'package:poetlum/features/dependency_injection/presentation/widgets/init_dependencies.dart';
 import 'package:poetlum/features/firebase/presentation/widgets/init_crashlytics_widget.dart';
 import 'package:poetlum/features/firebase/presentation/widgets/init_firebase_widget.dart';
+import 'package:poetlum/features/internet_connection_monitoring/presentation/widgets/init_network_controller.dart';
 import 'package:poetlum/features/multi_bloc_provider/presentation/init_blocs.dart';
 import 'package:poetlum/features/theme_change/presentation/widgets/init_theme_widget.dart';
 import 'package:poetlum/firebase_options.dart';
@@ -11,13 +12,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    InitFirebaseWidget(
-      options: DefaultFirebaseOptions.currentPlatform,
-      child: const InitDependencies(
-        child: InitCrashlyticsWidget(
-          child: InitBlocs(
-            child: InitTheme(
-              child: PoetlumApp(),
+    InitNetworkController(
+      child: InitFirebaseWidget(
+        options: DefaultFirebaseOptions.currentPlatform,
+        child: const InitDependencies(
+          child: InitCrashlyticsWidget(
+            child: InitBlocs(
+              child: InitTheme(
+                child: PoetlumApp(),
+              ),
             ),
           ),
         ),
