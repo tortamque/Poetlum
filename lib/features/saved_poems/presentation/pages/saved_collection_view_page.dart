716d4b73c95775ba5fb0cfc0c36c0b2e6d8234be
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-13 13:51:49 +0200

    Add collection name

--- lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
+++ lib/features/saved_poems/presentation/pages/saved_collection_view_page.dart
@@ -5,6 +5,7 @@ import 'package:poetlum/features/application/presentation/widgets/app_bar/app_ba
 import 'package:poetlum/features/application/presentation/widgets/loader.dart';
 import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/poems_feed/domain/repository/user_repository.dart';
+import 'package:poetlum/features/poems_feed/presentation/widgets/animations/top_animation.dart';
 import 'package:poetlum/features/saved_poems/domain/entities/collection.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_state.dart';
@@ -36,41 +37,37 @@ class _SavedCollectionViewPageState extends State<SavedCollectionViewPage> {
   Future<void> initPoems() async {
     poemsInTheCollection = await context.read<FirebaseDatabaseCubit>().getPoemsInCollection(
       userId: getIt<UserRepository>().getCurrentUser().userId!, 
-      collectionName: collectionEntity.isAllSavedPoems
-        ? null
-        : collectionEntity.name,
+      collectionName: collectionEntity.isAllSavedPoems ? null : collectionEntity.name,
     );
   }
   
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final localContext = context;
 
     return Scaffold(
       appBar: const CustomAppBar(title: 'Poetlum'),
-      floatingActionButton: collectionEntity.isAllSavedPoems
-        ? null 
-        : FloatingActionButton(
-          tooltip: 'Edit a collection',
-          onPressed: () async {
-            final savedPoems = await context.read<FirebaseDatabaseCubit>().getUserPoems(
-              getIt<UserRepository>().getCurrentUser().userId!,
-            );
+      floatingActionButton: collectionEntity.isAllSavedPoems ? null : FloatingActionButton(
+        tooltip: 'Edit a collection',
+        onPressed: () async {
+          final savedPoems = await context.read<FirebaseDatabaseCubit>().getUserPoems(
+            getIt<UserRepository>().getCurrentUser().userId!,
+          );
 
-            if (mounted){
-              await showModalBottomSheet(
-                context: localContext, 
-                isScrollControlled: true,
-                builder:(context) => UpdateCollectionBottomSheetContent(
-                  collectionName: collectionEntity.name ?? '',
-                  poemsInTheCollection: poemsInTheCollection,
-                  allSavedPoems: savedPoems,
-                ),
-              );
-            }
-          },
-          child: const Icon(Icons.edit),
-        ),
+          if (mounted){
+            await showModalBottomSheet(
+              context: localContext, 
+              isScrollControlled: true,
+              builder:(context) => UpdateCollectionBottomSheetContent(
+                collectionName: collectionEntity.name ?? '',
+                poemsInTheCollection: poemsInTheCollection,
+                allSavedPoems: savedPoems,
+              ),
+            );
+          }
+        },
+        child: const Icon(Icons.edit),
+      ),
       body: BlocConsumer<FirebaseDatabaseCubit, FirebaseDatabaseState>(
         listener: (context, state) async {
           if (state.status == FirebaseDatabaseStatus.needsRefresh) {
@@ -81,27 +78,80 @@ class _SavedCollectionViewPageState extends State<SavedCollectionViewPage> {
           if (state.status == FirebaseDatabaseStatus.submitting) {
             return const Loader(text: 'Snatching your poems from our top-secret database 🕵️‍♂️');
           } else {
-            return poemsInTheCollection.isEmpty
-              ? const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Center(
-                  child: Text(
-                    'Nothing to show here 😔\nTap on the "Edit" button to add amazing poems to the collection',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18),
-                  ),
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _CollectionName(name: collectionEntity.name),
+                Expanded(
+                  child: poemsInTheCollection.isEmpty
+                    ? const Center(
+                        child: Text(
+                          'Nothing to show here 😔\nTap on the "Edit" button to add amazing poems to the collection',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: poemsInTheCollection.length,
+                        itemBuilder: (__, index) => SavedPoemCard(
+                          poemEntity: poemsInTheCollection[index],
+                          collectionEntity: collectionEntity,
+                        ),
+                      ),
                 ),
-              )
-              : ListView.builder(
-                itemCount: poemsInTheCollection.length,
-                itemBuilder: (__, index) => SavedPoemCard(
-                  poemEntity: poemsInTheCollection[index],
-                  collectionEntity: collectionEntity,
-                ),
-              );
+              ],
+            );
           }
         },
       ),
     );
   }
+}
+
+class _CollectionName extends StatefulWidget {
+  const _CollectionName({required this.name});
+
+  final String? name;
+
+
+  @override
+  State<_CollectionName> createState() => __CollectionNameState();
+}
+
+class __CollectionNameState extends State<_CollectionName> {
+  bool isNameAnimated = false;
+  final Duration animationDelay = const Duration(milliseconds: 200);
+
+  void _startAnimations() {
+    final setters = <Function(bool)>[
+      (val) => isNameAnimated = val,
+    ];
+
+    for (var i = 0; i < setters.length; i++) {
+      Future.delayed(animationDelay * (i + 1)).then(
+        (_) => setState(() => setters[i](true)),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimations();
+  }
+
+  @override
+  Widget build(BuildContext context) => TopAnimation(
+    animationField: isNameAnimated,
+    positionInitialValue: MediaQuery.of(context).size.height/14,
+    child: Align(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        child: Text(
+          widget.name ?? 'Collection',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+  );
 }
