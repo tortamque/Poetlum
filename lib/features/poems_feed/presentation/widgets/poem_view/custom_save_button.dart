ea3a28bde13992831a4447e31c4d89e09df15173
tortamque <90132962+tortamque@users.noreply.github.com>
2023-12-17 11:40:08 +0200

    Move poem card

--- lib/features/poems_feed/presentation/widgets/poem_view/custom_save_button.dart
+++ lib/features/poems_feed/presentation/widgets/poem_view/custom_save_button.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:like_button/like_button.dart';
 import 'package:poetlum/core/dependency_injection.dart';
-import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/core/shared/domain/repository/user_repository.dart';
+import 'package:poetlum/features/poems_feed/domain/entities/poem.dart';
 import 'package:poetlum/features/saved_poems/presentation/bloc/firebase_database_cubit.dart';
 
 class CustomSaveButton extends StatefulWidget {
